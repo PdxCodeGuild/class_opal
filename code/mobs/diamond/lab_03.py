dd2974fad803b3e57174b07ef5b2815399c7dc7c
DJ Pettiford <dj620030@gmail.com>
2022-11-02 12:32:27 -0700

    lab_03 ver 2

--- code/mobs/diamond/lab_03.py
+++ code/mobs/diamond/lab_03.py
@@ -1,8 +1,39 @@
-nums = [5, 0, 8, 3, 4, 1, 6]
-sum_of_nums = 0
+# Ask the user to enter the numbers one at a time, putting them into a list. If the user enters 'done', then calculate and display the average. The following code demonstrates how to add an element to the end of a list.
 
-for num in nums:
-    sum_of_nums += num
+# ```python
+# nums = []
+# nums.append(5)
+# print(nums)
+# ```
 
-average = sum_of_nums/len(nums)
-print(average)
+# Below is an example input/output:
+
+
+# ```
+# > enter a number, or 'done': 5
+# > enter a number, or 'done': 3
+# > enter a number, or 'done': 4
+# > enter a number, or 'done': done
+# average: 4
+
+
+# nums = [5, 0, 8, 3, 4, 1, 6]
+# sum_of_nums = 0
+
+# for num in nums:
+#     sum_of_nums += num
+
+# average = sum_of_nums/len(nums)
+# print(average)
+
+
+num_list = []
+average = sum(num_list)/len(num_list)
+
+
+while True:
+    user_input = input("Enter number or 'done' to quit: ")
+    user_input = int(user_input)
+    if user_input == 'done':
+        print(f'The average is: {average}')
+        break
