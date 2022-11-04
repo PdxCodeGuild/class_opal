5ed0a66dab259efa0113cbfc7064eaa338526a24
elizabethquinlan <116927091+elizabethquinlan@users.noreply.github.com>
2022-11-04 15:20:39 -0700

    lizzie's python lab 1 (#21)
    * lizzie's python lab 1
    
    * lizzie python lab01 with edit

---
+++ code/lizzie/python/lab_1.py
@@ -0,0 +1,22 @@
+conversion_dict = {
+    'ft': 0.3048,
+    'mi': 1609.34,
+    'm': 1,
+    'km': 1000,
+    'yd': 0.9144,
+    'in': 0.0254
+}
+
+distance = input('What is the distance? ')
+
+distance=int(distance)
+
+input_unit = input('What are the input units? ')
+
+meter_conversion = conversion_dict[input_unit] * distance
+
+output_unit = input('What are the output units? ')
+
+conversion2 = meter_conversion / conversion_dict[output_unit]
+
+print(f'{distance} {input_unit} is {round(conversion2, 2)} {output_unit}.')
