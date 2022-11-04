d7bca03805e479b686c71f633f0ef543dda501b3
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-04 14:20:04 -0700

    add josh python lab 5 v2

--- code/josh/python/josh_lab_5_v2.py
+++ code/josh/python/josh_lab_5_v2.py
@@ -3,6 +3,7 @@
 from random import randint
 
 
+# Creates a function to return a list of 6 random integers from 1 to 99
 def pick6():
     return [randint(1, 99) for num in range(6)]
 
@@ -10,6 +11,7 @@ def pick6():
 winning_numbers = pick6()
 
 
+# Creates a function to compare the value and position of integer matches between 2 lists.  Increases the 'balance' variable based upon matches.
 def num_matches(winning, ticket):
     balance = 0
     match = 0
@@ -34,22 +36,21 @@ def num_matches(winning, ticket):
 
 
 total = 0
+expenses = 0
 
+# Loops through 'num_matches' function 100k times, decreasing total $2 for each ticket purchase, increasing total based upon winnings, and increasing expenses by $-2. 
 for pick in range(100000):
     total += num_matches(winning_numbers, pick6())
     total -= 2
+    expenses -= 2
 
 print(f"Your final balance is ${total}.")
 
+# Version 2 - ROI
 
-# Version 2
-# The ROI (return on investment) is defined as (earnings - expenses)/expenses. Calculate your ROI, print it out along with your earnings and expenses.
-
-# earnings = num_matches(winning_numbers, pick6())
-# expenses = pick - num_matches(winning_numbers, pick6())
-# ROI = (earnings - expenses)/expenses
-# print(f'''
-# Your earninngs are {earnings}.
-# Your expenses are {expenses}.
-# Your return on investment is {ROI}.
-# ''')
+ROI = total / (expenses * -1)
+print(f'''
+Your earnings are {total + (expenses * -1)}.
+Your expenses are {expenses}.
+Your return on investment is {ROI}.
+''')
