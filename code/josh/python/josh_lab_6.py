5ae80e795f77436749f535d2c232f1e4b9977139
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-04 19:30:05 -0700

    add josh lab 6 progress to lab 6

--- code/josh/python/josh_lab_6.py
+++ code/josh/python/josh_lab_6.py
@@ -1,7 +1,5 @@
 # Lab 6: Credit Card Validation
 
-# Let's write a function credit_card_validator which returns whether a string containing a credit card number is valid as a boolean. The steps are as follows:
-
 
 def credit_card_validator():
     user_input = list(input("Please enter credit card number with no spaces: "))
@@ -14,23 +12,30 @@ def credit_card_validator():
             credit_card_number[i] = credit_card_number[i] * 2
         else:
             credit_card_number[i] = credit_card_number[i] * 1
+    ### Doesn't work at all###
     # for num in credit_card_number:
     #     if num > 9:
     #         num = num - 9
     #     else:
-    #         num = num
-
-
-    credit_card_number = [i - 9 for i in credit_card_number if i > 9]
-    #list_sum = sum(credit_card_number)
+    #         num = num - 0
+    ###Works on numbers greater than 9, but does not display whole list afterwards###
+    # credit_card_number = [i - 9 for i in credit_card_number if i > 9]
+    ###print statements for review while developing code###
     print(check_digit)
-    #print(list_sum)
+    # print(list_sum)
     print(credit_card_number)
+    ###End of program, if previous code works###
+    # list_sum = sum(credit_card_number)
+    # for num in list_sum[1]:
+    #     if num == check_digit:
+    #         print("Credit card is valid!")
+    #     else:
+    #         print("Credit card is not valid!")
 
 
 credit_card_validator()
 
-
+###Point of progress in steps in program###
 # Subtract nine from numbers over nine.
 # Sum all values.
 # Take the second digit of that sum.
@@ -40,16 +45,7 @@ credit_card_validator()
 # Here is a valid credit card number to test with: 4556737586899855
 # For example, the worked out steps would be:
 
-# 4 5 5 6 7 3 7 5 8 6 8 9 9 8 5 5
-# 4 5 5 6 7 3 7 5 8 6 8 9 9 8 5
-# 5 8 9 9 8 6 8 5 7 3 7 6 5 5 4
-# 10 8 18 9 16 6 16 5 14 3 14 6 10 5 8
 # 1 8 9 9 7 6 7 5 5 3 5 6 1 5 8
 # 85
 # 5
 # True Valid!
-# Git Add, Commit & Push:
-# > git add files-to-be-added
-# > git commit -m "your commit message goes here"
-# > git push -u origin your_name-python-lab6
-# Then go to the repository to create a PR.
