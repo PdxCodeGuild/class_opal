2df489c590b1f0002c6391bccd6dd3d927ebb855
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-05 11:32:18 -0700

    add josh python lab 6 final draft

--- code/josh/python/josh_lab_6.py
+++ code/josh/python/josh_lab_6.py
@@ -10,37 +10,15 @@ def credit_card_validator():
     for i in range(len(credit_card_number)):
         if i % 2 == 0:
             credit_card_number[i] = credit_card_number[i] * 2
-        else:
-            credit_card_number[i] = credit_card_number[i] * 1
     for i in range(len(credit_card_number)):
         if credit_card_number[i] > 9:
             credit_card_number[i] = credit_card_number[i] - 9
-    ###print statements for review while developing code###
-    print(check_digit)
-    # print(list_sum)
-    print(credit_card_number)
-    ###End of program, if previous code works###
-    # list_sum = sum(credit_card_number)
-    # for num in list_sum[1]:
-    #     if num == check_digit:
-    #         print("Credit card is valid!")
-    #     else:
-    #         print("Credit card is not valid!")
+    list_sum = sum(credit_card_number)
+    comparison_digit = (check_digit * -1) - list_sum
+    if (comparison_digit * -1) - list_sum == check_digit:
+        print("Credit card is valid!")
+    else:
+        print("Credit card is not valid!")
 
 
 credit_card_validator()
-
-###Point of progress in steps in program###
-# Subtract nine from numbers over nine.
-# Sum all values.
-# Take the second digit of that sum.
-# If that matches the check digit, the whole card number is valid.
-
-
-# Here is a valid credit card number to test with: 4556737586899855
-# For example, the worked out steps would be:
-
-# 1 8 9 9 7 6 7 5 5 3 5 6 1 5 8
-# 85
-# 5
-# True Valid!
