4551c0ae341bd9bd0080282ebac232e6735551dd
NMurdaugh <nmurdaugh42@gmail.com>
2022-11-09 13:35:25 -0600

    nick lab08 version 2

--- code/nick/python/lab08.py
+++ code/nick/python/lab08.py
@@ -23,10 +23,37 @@ def valleys(data):
     return valleys
     
 
-def mountains(data):
-    ...
-    data.sort(reverse=True)
-    data_set = (data)
+list_of_lists =[]
+
+
+def mountains(data, lists):
+    highest_peak = max(data)
+    for num in reversed(range(1, highest_peak+1)):
+        string_of_chars = ' ' * len(data)
+        list_of_chars = list(string_of_chars)
+        for i, d in enumerate(data):
+            if d >= num:
+                list_of_chars[i] = 'X'
+        
+        lists.append(list_of_chars)
+    output_list = ""
+    for list_chars in lists:
+        output_list += " ".join(list_chars) + '\n'
+
+    data_string = ""
+    for d in data:
+        data_string += str(d)
+        
+    data_string = " ".join(data_string)
+    # print(output_list) #test
+    return f'''
+{output_list}
+{data_string}
+'''
+    
+
+
+        
 
 def flood():
     ...
@@ -36,9 +63,11 @@ peaks = peaks(data)
 valleys = valleys(data)
 points_of_interest = peaks + valleys
 points_of_interest.sort()
+mountains = mountains(data, list_of_lists)
 
 print(f'''
+{mountains}
 Peaks: {peaks}
 Valleys: {valleys}
-POI: {points_of_interest}''')
-print(set(data))
+POI: {points_of_interest}
+''')
