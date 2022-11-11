41be31175562c9beac721b738c95734532d1103a
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-10 16:40:08 -0800

    add josh python lab 11 step 1 progress to code

--- code/josh/python/josh_lab_11.py
+++ code/josh/python/josh_lab_11.py
@@ -1,26 +1,59 @@
 # Lab 11: Contact List
+relative_path = 'class_opal\code\josh\python\contacts.csv'
 
-# Let's build a program to manage a list of contacts. To start, we'll build a CSV ('comma separated values') together, and go over how to load that file. 
-# Headers might consist of name, favorite fruit, favorite color. Open the CSV, convert the lines of text into a list of dictionaries, one dictionary for 
-# each user. The text in the header represents the keys, the text in the other lines represent the values.
+with open(relative_path, 'r') as file:
+    lines:list = file.read().split('\n')
 
-# with open('contacts.csv', 'r') as file:
-#     lines = file.read().split('\n')
-#     print(lines)
+contacts = []
+contact_dict = {}
+
+keys: list[str] = lines[0].split(',')  
+
+for line in lines[1:]:
+    values: list[str] = line.split(',')
+    for key in keys:
+        contact_dict[key] = None
+        for value in values:
+            contact_dict[key] = value
+
+    # for value in values:
+    #     contact_dict[key] = value
+
+print(contact_dict)
+
+# for line in lines[1:]:
+#     values: list[str] = line.split(',')
+#     for key in keys:
+#         for value in values:
+#             if key not in contact_dict:
+#                 contact_dict[key] = value
+
+
+# for line in lines[1:]:
+#     values = line.split(',')
+#     print(values)
+
+# Create a dictionary for each key:value pair in keys and values.
+# Append dictionary to empty list 'contacts'
+
+
+# Convert the list of strings into a list of dictionaries with one dictionary per user. 
+# The text in the header represents the keys, the text in the other lines represent the values.
+
 # Once you've processed the file, your list of contacts will look something like this...
 
 # contacts = [
 #     {'name':'matthew', 'favorite fruit':'blackberries', 'favorite color':'orange'},
 #     {'name':'sam', 'favorite fruit':'pineapple' ...}
 # ]
-# Note: There is a csv library in Python that will do much of this for you. It is what you would use normally in a project, but for this lab you need to write 
-# all the logic yourself.
+
 
 # Step 2 - Implement a CRUD REPL
 # Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
 # Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
 # Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
 # Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list.
+
 
 # Step 3
 # When REPL loop finishes, write the updated contact info to the CSV file to be saved. I highly recommend saving a backup contacts.csv because you likely won't 
