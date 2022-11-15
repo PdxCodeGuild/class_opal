82ed5a3dfda276e39435945cf2ea23839d7913e8
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-14 16:31:01 -0800

    add josh python lab 11 CRUD progress to code

--- code/josh/python/josh_lab_11.py
+++ code/josh/python/josh_lab_11.py
@@ -5,64 +5,40 @@ with open(relative_path, 'r') as file:
     lines:list = file.read().split('\n')
 
 contacts = []
-contact_dict = {}
 
 keys: list[str] = lines[0].split(',')  
 
 for line in lines[1:]:
+    contact_dict = {}
     values: list[str] = line.split(',')
-    # value = values.split(',')
-    # value = [value.split(',') for value in values]
-    for key in keys:
-        contact_dict[key] = None
-    # for value in values:
-        # contact_dict.update({key: value})
-        # contact_dict[key] = value
-
-# class ContactDict:
-#     def __init__(self, key, value):
-#         self.key = key
-#         self.value - value
-    
-# for key in keys:
-#     key = ContactDict.key
-
-# for value in values:
-#     value = ContactDict.value
-
-# print(key, value)
-
-
-# print(contact_dict)
-
-# for line in lines[1:]:
-#     values: list[str] = line.split(',')
-#     for key in keys:
-#         for value in values:
-#             if key not in contact_dict:
-#                 contact_dict[key] = value
-
-
-# for line in lines[1:]:
-#     values = line.split(',')
-#     print(values)
-
-# Create a dictionary for each key:value pair in keys and values.
-# Append dictionary to empty list 'contacts'
-
-
-# Convert the list of strings into a list of dictionaries with one dictionary per user. 
-# The text in the header represents the keys, the text in the other lines represent the values.
-
-# Once you've processed the file, your list of contacts will look something like this...
-
-# contacts = [
-#     {'name':'matthew', 'favorite fruit':'blackberries', 'favorite color':'orange'},
-#     {'name':'sam', 'favorite fruit':'pineapple' ...}
-# ]
-
+    for i, key in enumerate(keys):
+        contact_dict[key] = values[i]
+    contacts.append(contact_dict)
 
 # Step 2 - Implement a CRUD REPL
+
+while True:
+    name = input('Enter a new name or type \'q\' to exit: ')
+    if name == 'q':
+        break
+    state = input('Enter a new state abbreviation: ')
+    branch = input('Enter a new military service abbreviation: ')
+    new_contact = [name, state, branch]
+    # contact_dict.update(new_contact)
+    for i, key in enumerate(keys):
+        contact_dict[key] = new_contact[i]######### Currently replacing last dictionary in list instead of adding new dictionary to end of list ###########
+    # contacts.append(contact_dict)
+
+    # for user_entry in new_contact:
+    #     contact_dict[0] = name
+    #     contact_dict[1] = state
+    #     contact_dict[2] = branch
+    #     # i, key in enumerate(keys):
+    #     # contact_dict[key] = new_contact[i]
+    # contacts.append(new_contact)
+    
+    print(contacts)
+
 # Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
 # Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
 # Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
