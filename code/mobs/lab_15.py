090d4c6d5867e007fbb9fe8c66e3a37865e8b583
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-15 14:55:40 -0800

    driver switches

--- code/mobs/lab_15.py
+++ code/mobs/lab_15.py
@@ -1,34 +1,32 @@
 # # Lab 15: Tic-Tac-Toe 
 
-# [Tic Tac Toe](https://en.wikipedia.org/wiki/Tic-tac-toe) is a game.
 # Players take turns placing tokens (a 'O' or 'X') into a 3x3 grid.
 # Whoever gets three in a row first wins.
 
-# You will write a **Player** class and **Game** class to model Tic Tac Toe, and a function **main**
-#  that models gameplay taking in user inputs through REPL.
+# You will write a **Player** class and **Game** class to model Tic Tac Toe, and a function **main** that models gameplay taking in user inputs through REPL.
 
 
-# The Player class has the following properties: 
-# * **name** = *player name*
-# * **token** = *'X' or 'O'*
+class Game:
+    def __init__(self):
+        self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
-# The Game class has the following properties:
-# * **board** = *your representation of the board*
 
-# You can represent the board however you like, such as a 2D list, tuples, or dictionary.
+    def __repr__(self):
+        '''Returns a pretty string representation of the game board'''
+        new_board = []
+        for i, string in enumerate(self.board):
+            string = '|'.join(self.board[i])
+            new_board.append(string)
+        return "\n".join(new_board)
 
-# The Game class has the following methods:
-# * `__repr__()` Returns a pretty string representation of the game board
-# ```py
-# >>> print(board)
-# X| | 
-# O|X|O
-#  | | 
-# ```
 
-# * `move(x, y, player)` Place a player's token character string at a given coordinate 
-# (top-left is 0, 0),
-#  x is horizontal position, y is vertical position.
+    def move(self): # , x, y, player):
+        '''Place a player's token character string at a given coordinate 
+(top-left is 0, 0), x is horizontal position, y is vertical position.'''
+        x = 0
+        y = 1
+        player = 'X'
+        self.board[y][x] = player
 
 # ```py
 # >>> board.move(2, 1, player_1)
@@ -37,8 +35,12 @@
 #  | | 
 # ```
 
-# * `calc_winner()` What token character string has won or `None` if no one has.
+        ...
 
+
+    def calc_winner(self):
+        ''' What token character
+string has won or `None` if no one has.'''
 # ```py
 # X| | 
 # O|X|O
@@ -47,8 +49,12 @@
 # X
 # ```
 
-# * `is_full()` Returns true if the game board is full.
+        ...
 
+
+    def is_full(self):
+        '''Returns true if the game board is full.'''
+        ...
 # ```py
 # X|O|X
 # X|X|O
@@ -57,8 +63,11 @@
 # True
 # ```
 
-# * `is_game_over()` Returns true if the game board is full or a player has won.
 
+    def game_over(self):
+        '''Returns true if the game board
+is full or a player has won.'''
+        ...
 # ```py
 # X|O|X
 # X|X|O
@@ -73,37 +82,13 @@
 # False
 # ```
 
-class Game:
-    def __init__(self):
-        self.board = [' | | ',' | | ',' | | ',]
-
-    def __repr__(self):
-        '''Returns a pretty string representation of the game board'''
-        return "\n".join(self.board)
-
-    def move(self, x, y, player):
-        '''Place a player's
-token character string at a given coordinate 
-(top-left is 0, 0), x is horizontal position, y is vertical position.'''
-        ...
-    
-    def calc_winner(self):
-        ''' What token character
-string has won or `None` if no one has.'''
-        ...
-
-    def is_full(self):
-        '''Returns true if the game board is full.'''
-        ...
-
-    def game_over(self):
-        '''Returns true if the game board
-is full or a player has won.'''
-        ...
-
 
 game = Game()
 print(game)
+game.move()
+print(game)
+
 # class Player:
 #     def __init__(self, name, token):
-#         ...
+        #name = player_name
+        #token = 'X' or 'O'
