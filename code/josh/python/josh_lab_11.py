de2332f0f6361d6eafdea2f0a02f7d7f8859f914
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-16 13:47:23 -0800

    add josh python lab 11 final to code

--- code/josh/python/josh_lab_11.py
+++ code/josh/python/josh_lab_11.py
@@ -1,13 +1,15 @@
 # Lab 11: Contact List
 relative_path = 'class_opal\code\josh\python\contacts.csv'
 
+# Opens file at 'relative_path' and converts lines into a list of strings
 with open(relative_path, 'r') as file:
     lines:list = file.read().split('\n')
 
 contacts = []
-
+# Creates a list of strings, where each string will be used as a dictionary key from the first line in the file above
 keys: list[str] = lines[0].split(',')  
 
+# Iterates through the remaining list strings and converts each string to a dictionary value 
 for line in lines[1:]:
     contact_dict = {}
     values: list[str] = line.split(',')
@@ -62,18 +64,20 @@ while True:
                 pass
     
     
-# Step 3 - Write the updated contact info to the CSV file to be saved
 contacts_output = []
 
+# Places keys from dictionary back into index '0' at the beginning of the list of lines from the file above
 key_info = [k for k in contacts[0].keys()]
 single_key = ','.join(key_info)
 contacts_output.append(single_key)
 
+# Places values from dictionary back into remaining indexes after '0' in the list of lines from the file above, including additional entries from program
 for contact in contacts:
     contact_info = [c for c in contact.values()]
     single_contact = ','.join(contact_info)
     contacts_output.append(single_contact)
 
+# Overwrites original file with information from 'contacts_ouput' list above 
 output_path = 'class_opal\code\josh\python\contacts.csv'
 output_content = '\n'.join(contacts_output)
 
