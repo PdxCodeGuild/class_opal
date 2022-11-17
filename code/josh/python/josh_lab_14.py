60c4f4c51dc075d6200bd7024afccdfefcc7df12
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-16 16:01:44 -0800

    add josh python lab 14 progress to code

--- code/josh/python/josh_lab_14.py
+++ code/josh/python/josh_lab_14.py
@@ -1,11 +1,41 @@
-# Lab 14: Automated Tests
+# Lab 14: Automated Tests - Peaks and Valleys, ARI, ATM
 
-# Write automated tests that run with pytest for the following labs:
-# Peaks and Valleys
-# ARI
-# ATM
+# Each functions tests should have multiple assertions. Test all of the edge cases you can think of, keeping assertions small.
+# For example, in peaks_and_valleys you might test [1, 2, 3, 2] and [3, 2, 3, 2] instead of long lists.
 
-# Each function should have it's own set of tests and each set of tests should have multiple assertions. Test all of the edge cases you can think of, 
-# but keep individual assertions small. For example, for peaks and valleys you might test [1, 2, 3, 2] and [3, 2, 3, 2] instead of long lists.
+########## PEAKS AND VALLEYS ##########
+from josh_lab_8 import peaks_and_valleys
+
+# # Creates a function to return a list of the peaks and valleys in order of appearance in the original data
+# def peaks_and_valleys(data):
+#     peaks_and_valleys_list = []
+#     for key in data:
+#         try:
+#             if data[key] == data[key + 2]:
+#                 peaks_and_valleys_list.append(key + 1)   
+#         except:
+#             break    
+#     print(peaks_and_valleys_list)
+
+
+# peaks_and_valleys(data_dict)
+
+
+########## AUTOMATED READABILITY INDEX ##########
+# from josh_lab_9 import ???
+
+
+########## AUTOMATED TELLER MACHINE ##########
+from josh_lab_12 import ATM
+
+def test_check_balance:
+def test_deposit:
+def test_check_withdrawal:
+def test_withdraw:
+def test_calc_interest:
+def test_print_transactions:
+
+
 
 # If you need to refactor your code to make it more testable, include the refactored code in your pull request.
+# To run pytest enter the following: 'python -m pytest josh_lab_14.py'
--- code/josh/python/josh_lab_8.py
+++ code/josh/python/josh_lab_8.py
@@ -1,43 +1,43 @@
 # Lab 8 - Peaks and Valleys, Version 1
 
-# data_dict = {
-#     0: 1,
-#     1: 2,
-#     2: 3,
-#     3: 4,
-#     4: 5,
-#     5: 6,
-#     6: 7,
-#     7: 6,
-#     8: 5,
-#     9: 4,
-#     10: 5,
-#     11: 6,
-#     12: 7,
-#     13: 8,
-#     14: 9,
-#     15: 8,
-#     16: 7,
-#     17: 6,
-#     18: 7,
-#     19: 8,
-#     20: 9
-# }
+data_dict = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4,
+    4: 5,
+    5: 6,
+    6: 7,
+    7: 6,
+    8: 5,
+    9: 4,
+    10: 5,
+    11: 6,
+    12: 7,
+    13: 8,
+    14: 9,
+    15: 8,
+    16: 7,
+    17: 6,
+    18: 7,
+    19: 8,
+    20: 9
+}
 
 
-# # Creates a function to return a list of the peaks and valleys in order of appearance in the original data
-# def peaks_and_valleys(data):
-#     peaks_and_valleys_list = []
-#     for key in data:
-#         try:
-#             if data[key] == data[key + 2]:
-#                 peaks_and_valleys_list.append(key + 1)   
-#         except:
-#             break    
-#     print(peaks_and_valleys_list)
+# Creates a function to return a list of the peaks and valleys in order of appearance in the original data
+def peaks_and_valleys(data):
+    peaks_and_valleys_list = []
+    for key in data:
+        try:
+            if data[key] == data[key + 2]:
+                peaks_and_valleys_list.append(key + 1)   
+        except:
+            break    
+    print(peaks_and_valleys_list)
 
 
-# peaks_and_valleys(data_dict)
+peaks_and_valleys(data_dict)
 
 
 
