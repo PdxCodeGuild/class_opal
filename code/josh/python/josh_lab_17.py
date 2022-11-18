022ca4eb4d957a60abaed0f23ebe5f4540bafaef
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-18 15:54:41 -0800

    add josh lab 17 progress to code

--- code/josh/python/josh_lab_17.py
+++ code/josh/python/josh_lab_17.py
@@ -17,18 +17,23 @@ header = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
 keyword = input('Enter a keyword to search for quotes or \'q\' to exit: ')
 search_response = requests.get('https://favqs.com/api/quotes?page=page&filter=' + keyword + '&type=tag', headers=header)
 search_response_text = search_response.json()
-author = search_response_text['quotes'][1]['author']
-quote = search_response_text['quotes'][1]['body']
+# author = search_response_text['quotes'][1]['author']
+quotes: list = search_response_text['quotes']
+# quote = search_response_text['quotes'][1]['body']
 page = search_response_text['page']
-print(f'25 quotes associated with {keyword} - page {page}')
-# print(search_response_text)
-print(f'\"{quote}\" - {author}')
-# for keystroke in keyword:
-#     if keyword == 'q':
-#         break
-#     else:
-#         print(f'{int} quotes associated with {keyword}.')
-#         print('<list of quotes>')
+
+if keyword == 'q':
+    exit
+else:
+    print(f'25 quotes associated with {keyword} - page {page}')
+    for i in quotes:
+        for key in i:
+            if key == 'author':
+                author = (i[key])
+            elif key == 'body':
+                quote = (i[key])
+        print(f'\"{quote}\"\n\t - {author}')
+
 # next = input('Enter \'next page\' or \'done\': ')
 # if next == 'next page':
 #     print(f'{int} quotes associated with {keyword}.')
@@ -36,8 +41,11 @@ print(f'\"{quote}\" - {author}')
 # elif next == 'done':
 #     continue
 # else:
-
 #     continue
+
+# pprint(quotes)
+# pprint(search_response_text)
+# print(f'\"{quote}\" - {author}')
 
 # Prompt the user for a keyword, list the quotes you get in response, and prompt the user to either show the next page or enter a new keyword. 
 # You can use string concatenation to build the URL.
