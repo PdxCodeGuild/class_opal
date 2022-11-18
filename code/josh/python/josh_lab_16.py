2f2a59905f8c88386326f7660f18065b3151c633
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-17 16:29:14 -0800

    add josh python lab 16 progress to code

--- code/josh/python/josh_lab_16.py
+++ code/josh/python/josh_lab_16.py
@@ -8,29 +8,19 @@ header = {'accept': 'application/json'}
 response = requests.get('https://icanhazdadjoke.com/', headers=header)
 response_text = response.json()
 
-# pprint(response_text)
-
-
-# Part 2 - Add the ability to "search" for jokes using another endpoint:
-parameters = {'page': int, 'limit': int, 'term': str} #page-location(def:1), limit-# of results(def:20,max:30), term-search term(default:all)
-
-search_response = requests.get('https://icanhazdadjoke.com/search/', params=parameters, headers=header, )
-search_response_text = search_response.json()
-
-pprint(search_response_text)
-
-# Create a REPL that allows one to enter a search term and go through jokes one at a time:
+# Part 2 - Create a REPL that allows user to enter a search term and go through jokes one at a time
 
 while True:
     user_input = input('Enter a search term to locate a related dad joke: ')    # user enters string to become search term to find joke
-    for element in parameters:  # loop through 'parameters' var to update 'term' in search
-        if element == 'term':   # stops on element 'term' in loop of 'parameters' var
-            user_input == :
-            print(search_response_text['joke'])
-        else:
-            print(f'{user_input} did not return any jokes.')    # displays to user if search term doesn't exist
-            pass
-
+    parameter = {'term': user_input}    # term - search term (default: all)
+    # search_response = requests.get('https://icanhazdadjoke.com/search/', params=parameter, headers=header)
+    search_response = requests.get(f'https://icanhazdadjoke.com/search?term={user_input}', headers=header)  # temp fix for line 16 'params' not working
+    search_response_text = search_response.json()
+    if search_response_text['status'] == 404:
+        print(f'{user_input} did not return any jokes.')    # displays to user if search term doesn't exist
+    else:
+        print(search_response_text)
+        pass
     again = input('Would you like another joke? (\'y\' for yes or \'n\' to exit) ') # asks user to continue or not
     if again == 'n':    # exits loop
         break
