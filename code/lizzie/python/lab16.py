bb433690c812c32c922e85819d12f8c3fdaf5368
lizzie <elizabeth.j.quinlan@gmail.com>
2022-11-18 12:07:31 -0800

    Lizzie python lab16

--- code/lizzie/python/lab16.py
+++ code/lizzie/python/lab16.py
@@ -1,54 +1,41 @@
 # Lab 16: Dad Joke API
 
-# Use the [Dad Joke API](https://icanhazdadjoke.com/api) to get a dad joke and 
-# display it to the user. You may want to also use [time.sleep](https://www.geeksforgeeks.org/sleep-in-python/) 
-# to add suspense.
-
-""" 
-
-# importing the request library.
 import requests
 
-# ## Part 1
 
-# Use the [requests](../docs/15%20Requests.md) library to send an HTTP request
-#  to `https://icanhazdadjoke.com/` with the `accept` header as `application/json`. 
-# This will return a dad joke in JSON format.
+#keeping everything in one loop.
+while True:
 
+    user_response = str(input("\nEnter a search term here or 'quit' to quit: "))
+    
+    if user_response == "quit" or user_response == "clear":
+        print(f'\nHow do you say goodbye to your two sons?\nBison.')
+        break
 
-# These all do roughly the same thing
-response = requests.get(
-    'https://icanhazdadjoke.com/', headers={'accept': 'application/json'})
+    parameters = {
+        'term': user_response
+    }
+    response = requests.get(
+        'https://icanhazdadjoke.com/search', params=parameters, headers={'accept': 'application/json'})
+    response_text = response.json()
 
+    # Telling the user how many jokes there are so they know how many to expect if they
+    # choose to sort through them. Also works well if no jokes are returned.
+    print(f"There are {response_text['total_jokes']} total joke(s) with this search term.")
 
-#  You can then use the `.json()` method 
-# # on the response to get a dictionary. Get the joke out of the dictionary and show it to the user.
-
-
-# Parse the text (JSON) response into a python dictionary
-# response_text = json.loads(response.text)
-response_text = response.json()
-
-
-pprint(response_text)
-
-
-# yikes = requests.get('https://icanhazdadjoke.com/')
-# print(yikes.text)
- """
-
-
-# ## Part 2
-
-# Add the ability to "search" for jokes using [another endpoint](https://icanhazdadjoke.com/api#search-for-dad-jokes).
-#  Create a REPL that allows one to enter a search term and go through jokes one at a time. 
-# You can also add support for multiple pages.
-
-""" while True:
-
-    response = input("\nEnter a search term here or 'done' to quit: ")
-
-    if response == 'done':
-        print(f'You entered {numbers}.')
-        print(f'The sum of the numbers is {sum(numbers)}')
-        break """
+    if user_response != "quit":
+        #Accessing the list of dicts inside 'results'
+        for dict in (response_text['results']):
+            #Acessing the key 'joke' inside the dict
+            print(dict['joke'])
+            next_joke_query = input("Would you like to see the next joke? y or n: ")
+            # The for loop will continue showing the next joke if the responses are affirmative
+            # Or repeat the search term the user was inputting earlier.
+            if next_joke_query == 'y' or next_joke_query == 'yes' or next_joke_query == user_response:
+                continue
+            elif next_joke_query == 'n' or next_joke_query == 'no':
+                print("\nI decided to quit my job as a personal trainer because\
+I'm not big enough or strong enough.\nI've handed in my Too Weak notice.")
+                break
+            else:
+                break
