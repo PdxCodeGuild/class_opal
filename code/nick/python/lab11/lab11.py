b5ec2e5827a85ba20a7924d499c32f8a5681d4d3
Danny Burrow <danny@pdxcodeguild.com>
2022-11-17 16:20:51 -0800

    refactor

--- code/nick/python/lab11/lab11.py
+++ code/nick/python/lab11/lab11.py
@@ -1,38 +1,35 @@
 from os import system
-relative_path = 'class_opal/code/nick/python/lab11/contacts.csv'
+relative_path = 'code/nick/python/lab11/contacts.csv'
 
 
-def crud():
-    acceptable_input = ['create', 'view', 'update', 'delete', 'done']
-    contacts, headers = unpack_contacts(relative_path)
-    names = []
-    for contact in contacts:
-        names.append(contact['name'])
-    names = '\n'.join(names)
-
-    while True:
-        print(names)
-
-        command = input("""
+MENU = """
 Type 'create' to add a new contact. 
 Type 'view' to retrieve and view an existing contact.
 Type 'update' to update an existing contact.
 Type 'delete' to delete an existing contact.
 Or type 'done' to finish.
-""").lower()
+"""
 
+
+def crud():
+    acceptable_input = ['create', 'view', 'update', 'delete', 'done']
+    contacts, headers = unpack_contacts(relative_path)
+    names = '\n'.join([c['name'] for c in contacts])
+
+    while True:
+        print(names)  # I wouldn't do this, just for UX reasons
+        command = input(MENU).lower()
         if command in acceptable_input:
             break
         else:
             print('That is an invalid input.')
-
 
     match command:
         case 'create':
             create_contact(contacts, headers)
             crud()
         case 'view':
-            view_contact(contacts, headers, command)
+            view_contact(contacts, command)
             crud()
         case 'update':
             update_contact(contacts, headers, command)
@@ -54,32 +51,25 @@ def unpack_contacts(path=relative_path):
 
     # remove the headers from the data and store them where we can work with them separately
     contacts_lines: list = contacts.split('\n')
-    contacts_headers = contacts_lines.pop(0).split(',')
-    #establish contacts list
+    headers = contacts_lines.pop(0).split(',')
+
     contacts = []
-    
     for line in contacts_lines:
         line = line.split(',')
-        dict = {}
-        for i, header in enumerate(contacts_headers):
-            dict[header] = line[i]
-        contacts.append(dict)
+        contact_dict = {headers[i]: line[i] for i in range(len(headers))}
+        # another version of the same thing
+        # contact_dict = {header: line[i] for i, header in enumerate(headers)}
+        contacts.append(contact_dict)
 
-    return contacts, contacts_headers
+    return contacts, headers
 
 
 def pack_contacts(contacts, headers, path=relative_path):
     '''
     packs the contacts dictionary back into a properly formatted CSV file
     '''
-    contacts = contacts
     headers = ','.join(headers)
-    contact_lines = []
-    for contact in contacts:
-        contact_line = []
-        for header in contact:
-            contact_line.append(contact[header])
-        contact_lines.append(','.join(contact_line))
+    contact_lines = [','.join(c.values()) for c in contacts]
 
     contact_lines.insert(0, headers)
     contacts = '\n'.join(contact_lines)
@@ -89,9 +79,9 @@ def pack_contacts(contacts, headers, path=relative_path):
 
     system('cls||clear')
     print('Changes committed.\n\n')
-        
-        
-def retrieve_contact(contacts, command='view'):
+
+
+def retrieve_contact(contacts, command='view') -> dict:
     '''
     return a contact dictionary given the name of a contact from the user
     '''
@@ -103,25 +93,24 @@ Enter the name which you would like to {command} from the list above.
             return contacts[i]
 
 
-def create_contact(contacts, headers):
+def create_contact(contacts: list[dict], headers: list[str]):
     '''
     create a new contact with all info and save it to the CSV
     '''
     new_contact = {}
     for header in headers:
-            new_contact[header] = input(f"Enter the contact's {header}:  ")
-        
+        new_contact[header] = input(f"Enter the contact's {header}:  ")
+
     contacts.append(new_contact)
     pack_contacts(contacts, headers, relative_path)
-    
 
-def view_contact(contacts, headers, command):
+
+def view_contact(contacts: list[dict], command: str):
     '''
     View a contact given a name from the user. 
     Give the option to delete, update, or go back to the main menu
     '''
-    contact = retrieve_contact(contacts, command)
-    # print(contact)
+    contact: dict = retrieve_contact(contacts, command)
     system('cls||clear')
     for key, val in contact.items():
         print(f'{key}: {val}')
@@ -129,13 +118,9 @@ def view_contact(contacts, headers, command):
     print('\n\n')
 
 
-def update_contact(contacts, headers, command):
-    contacts = contacts
-    headers = headers
-    contact = retrieve_contact(contacts, command)
-    for i, record in enumerate(contacts):
-        if contact == record:
-            contact = contacts.pop(i)
+def update_contact(contacts: list[dict], headers: list[str], command: str):
+    contact: dict = retrieve_contact(contacts, command)
+    contacts.remove(contact)
 
     print('Type the key you would like to update from the following list.\n')
     for key in contact.keys():
@@ -148,13 +133,11 @@ def update_contact(contacts, headers, command):
     pack_contacts(contacts, headers, relative_path)
 
 
-def delete_contact(contacts, headers, command):
+def delete_contact(contacts: list[dict], headers: list[str], command: str):
     '''
     deletes a contact that is already selected
     '''
-    contact = retrieve_contact(contacts, command)
-    contacts = contacts
-    headers = headers
+    contact: dict = retrieve_contact(contacts, command)
     for i, record in enumerate(contacts):
         if contact == record:
             contacts.remove(contacts[i])
