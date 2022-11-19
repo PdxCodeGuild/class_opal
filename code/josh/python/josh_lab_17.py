bf1b2527ceb8d247491c24fda0a8ed99341e4a5e
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-18 20:32:54 -0800

    add josh python lab 17 final to code

--- code/josh/python/josh_lab_17.py
+++ code/josh/python/josh_lab_17.py
@@ -12,51 +12,33 @@ quote = response_text['quote']['body']
 
 
 # Version 2: List Quotes by Keyword
+keyword = input('Enter a keyword to search for quotes or \'q\' to exit: ')  # stores user input in var 'keyword' outside loop to allow next page results to load
+page = 1    # 'page' counter outside the loop to allow counter to increase with each loop
 
 while True:
     header = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
-    keyword = input('Enter a keyword to search for quotes or \'q\' to exit: ')
-    page = 1
-    parameters = {'filter': keyword, 'page': page}
+    parameters = {'filter': keyword, 'page': page}  # stores user input and page number to allow search terms and next page functionality in URL
     search_response = requests.get('https://favqs.com/api/quotes', params=parameters, headers=header)
     search_response_text = search_response.json()
-    quotes: list = search_response_text['quotes']
-    page = search_response_text['page']
-    next_page = int(search_response_text['page'] + 1)
-    if keyword == 'q':
+    quotes: list = search_response_text['quotes']   # sorts returned list index 'quotes'
+    if keyword == 'q':  # user input ends search/program
         break
     else:
         print(f'25 quotes associated with {keyword} - page {page}')
-        for i in quotes:
-            for key in i:
+        for i in quotes:    # loops through list indices (list of dictionaries)
+            for key in i:   # loops through dictionary keys
                 if key == 'author':
-                    author = (i[key])
+                    author = (i[key])   # stores value of key 'author' as 'author'
                 elif key == 'body':
-                    quote = (i[key])
-            print(f'\"{quote}\"\n\t - {author}')
-
-    next = input('Enter \'next page\' or \'done\': ')
+                    quote = (i[key])    # stores value of key 'body' as 'quote'
+            print(f'\"{quote}\"\n\t - {author}')    # prints loop values of authors and quotes
+    next = input('Enter \'next page\' or \'done\': ')   # stores user input to allow exiting search/program or to check results of next page returned
     if next == 'done':
-        print('Goodbye.')
+        print('Thank you for searching for quotes.  We hope you were inspired.  Good day.  I said good day!')
         break
     elif next == 'next page':
-        page == next_page
+        page += 1   # increases page number to return next page of results for search based upon user input
         continue
     else:
-        print('You did not enter a valid response.  Goodbye.')
+        print('You did not enter a valid response.  Goodbye.')  # exits program
         break
-# else:
-#     continue
-
-# pprint(quotes)
-# pprint(search_response_text)
-# print(f'\"{quote}\" - {author}')
-
-# Prompt the user for a keyword, list the quotes you get in response, and prompt the user to either show the next page or enter a new keyword. 
-# You can use string concatenation to build the URL.
-
-# Optional URL Parameters:
-
-# Parameter	Description
-# filter	Type lookup or keyword search
-# page	Page number (25 quotes per page)
