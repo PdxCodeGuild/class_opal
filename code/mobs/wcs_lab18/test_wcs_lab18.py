88665ef5e5cef0ba90c573a83560556ebd81f139
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-21 14:54:04 -0800

    driver switch

--- code/mobs/wcs_lab18/test_wcs_lab18.py
+++ code/mobs/wcs_lab18/test_wcs_lab18.py
@@ -1,8 +1,16 @@
 import wcs_lab18
-from wcs_lab18 import SOCK_TYPES, Sorter
+from wcs_lab18 import rand_sock, find_pairs, SOCK_TYPES
 
 
-def test_socks_sorter():
-    sorter = Sorter()
-    assert len(sorter.rand_sock()) is 100
+def test_rand_sock():
+    sock = rand_sock()
+    assert len(sock) is 100
+    for s in sock:
+        assert s in SOCK_TYPES
+    assert 'ankle' and 'crew' in sock
+   
+def test_find_pairs():
+    assert type(find_pairs()) == dict
+    assert 
+
 # loop over list and test that each item is in SOCK_TYPES
--- code/mobs/wcs_lab18/wcs_lab18.py
+++ code/mobs/wcs_lab18/wcs_lab18.py
@@ -4,40 +4,24 @@ import random
 SOCK_TYPES = ['ankle', 'crew', 'calf', 'thigh']
 
 
-class Sorter:
-    def __init__(self):
-        self.socks = self.rand_sock()
+def rand_sock():
+    sock_list = random.choices(SOCK_TYPES, k=100)
+    return sock_list
 
-    def rand_sock(self):
-        self.socks = random.choices(SOCK_TYPES, k=100)
-        # sock_sorter += random.choices(SOCK_TYPES, k=100)
-        return self.socks
-# # random.choices(sequence, weights=None, cum_weights=None, k=1)
-#         for sock in range(len(SOCK_TYPES)+1):
-#             sock_sorter += list(combinations(SOCK_TYPES, sock))
-        # print(sock_sorter)
-        # itertools.combinations(SOCK_TYPES, 100)
+def find_pairs():
+    sock_drawer = {}
+    print(sock_drawer)
+    for sock_type in SOCK_TYPES:
+        sock_drawer[sock_type] = sock_type
+    return sock_drawer
 
-# Sorter.rand_sock(SOCK_TYPES)
+        # {sock_type: count}
 
-# from itertools import combinations
-# sample_list = ['a', 'b', 'c']
-# list_combinations = list()
-# for n in range(len(sample_list) + 1):
-#     list_combinations += list(combinations(sample_list, n))
-# print(list_combinations)
+print(find_pairs())
 
 
-# Lab: Sock Sorter
-
-# You've just finished laundry and your expansive sock
-
-# collection is in complete disorder. Sort your socks into
-# pairs and pull out any loners.
-
-# 1) Generate a list of 100 random socks, randomly
-# chosen from a set of types: `sock_types = ['ankle',
-# 'crew', 'calf', 'thigh']`
+# Find number of duplications and loners for each sock type. 
+# Sort your socks into pairs and pull out any loners.
 
 # 2) Find the number of duplicates and loners for each sock
 #  type. Hint: dictionaries are helpful here.
@@ -51,3 +35,12 @@ class Sorter:
 # group them into pairs.
 
 # `sock_colors = ['black', 'white', 'blue']`
+
+
+
+# from itertools import combinations
+# sample_list = ['a', 'b', 'c']
+# list_combinations = list()
+# for n in range(len(sample_list) + 1):
+#     list_combinations += list(combinations(sample_list, n))
+# print(list_combinations)
