11bd02f2415233373b64b12d1dd8872e8916536c
NMurdaugh <nmurdaugh42@gmail.com>
2022-11-21 18:31:07 -0600

    wip 21NOV

--- code/nick/python/lab16.py
+++ code/nick/python/lab16.py
@@ -7,12 +7,23 @@ def random_joke():
     ...
 
 
+def search_joke():
+    page = 1
+    search_term = input('Type the term for which you would like to search:')
+    parameters = {
+        'page': str(page),
+        'limit': '1',
+        'term': search_term
+    }
+    endpoint = 'search'
+    response = requests.get(f'https://icanhazdadjoke.com/{endpoint}', params=parameters,
+                            headers={'accept': 'application/json'})
+
+    pprint(response.json()['joke'])
+
+
 command = input(
     "Enter 'random' to get a random Dad Joke or 'search' to search for dad jokes using a keyword.").lower()
-
-
-endpoint = 'search'
-response = requests.get(f'https://icanhazdadjoke.com/{endpoint}',
-                        headers={'accept': 'application/json'})
-
-pprint(response.json()['joke'])
+match command:
+    case 'search':
+        search_joke()
