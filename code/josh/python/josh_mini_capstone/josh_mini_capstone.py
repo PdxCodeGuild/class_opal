c2c5e5d8252e3746c97098e01d9d4e4344410b79
Josh <24633211+JFC1@users.noreply.github.com>
2022-11-23 12:30:36 -0800

    add josh python mini-capstone progress to code

--- code/josh/python/josh_mini_capstone/josh_mini_capstone.py
+++ code/josh/python/josh_mini_capstone/josh_mini_capstone.py
@@ -14,7 +14,6 @@
     # There is more info on presentations in the README
 
 ######################################################################################################################################################
-######################################################################################################################################################
 
 import requests
 import pandas as pd
@@ -31,21 +30,21 @@ from urllib.request import urlopen
 # quote = response_text['quote']['body']
 
 
-# Opens first website 'Tax Foundation' to webscrape effective tax burdens
+# Opens 1st website to webscrape effective tax burdens
 with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
     soup_tax = BeautifulSoup(response, 'html.parser')
 
-# Opens second website '???' to webscrape political leanings
-with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
-    soup_tax = BeautifulSoup(response, 'html.parser')
+# Opens 2nd website (data from Pew Research Center, Kaiser Family Foundation, et al.) to webscrape political leanings by state
+with urlopen('https://worldpopulationreview.com/state-rankings/states-by-political-party') as response:
+    soup_politics = BeautifulSoup(response, 'html.parser')
 
-# Opens third website '???' to webscrape crime rates
-with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
-    soup_tax = BeautifulSoup(response, 'html.parser')
+# Opens 3rd website (data from FBI Crime Data Explorer) to webscrape crime rates by state
+with urlopen('https://worldpopulationreview.com/state-rankings/crime-rate-by-state') as response:
+    soup_crime = BeautifulSoup(response, 'html.parser')
 
-# Opens fourth website '???' to webscrape median housing values
-with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
-    soup_tax = BeautifulSoup(response, 'html.parser')
+# Opens 4th website (data from Zillow, et al.) to webscrape median housing values by state
+with urlopen('https://worldpopulationreview.com/state-rankings/median-home-price-by-state') as response:
+    soup_house = BeautifulSoup(response, 'html.parser')
 
 
 # Returns state, tax_rate, and data based upon user input for either state or rank
@@ -60,73 +59,90 @@ def tax_burden(user_input):
             return f'State: {state}, Effective Tax Burden: {tax_rate}, Rank: {rank}'
 
 
-##### Will likely remove later, if not used #####
-state_rank = {
-    '20': 'Alabama 9.8%', 
-    '1': 'Alaska 4.6%',
-    '15': 'Arizona 9.5%',
-    '26': 'Arkansas 10.2%',
-    '46': 'California 13.5%',
-    '19': 'Colorado 9.7%',
-    '49': 'Connecticut 15.4%',
-    '42': 'Delaware 12.4%',
-    '11': 'Florida 9.1%',
-    '8': 'Georgia 8.9%',
-    '48': 'Hawaii 14.1%',
-    '29': 'Idaho 10.7%',
-    '44': 'Illinois 12.9%',
-    '14': 'Indiana 9.3%',
-    '34': 'Iowa 11.2%',
-    '33': 'Kansas 11.2%',
-    '17': 'Kentucky 9.6%',
-    '12': 'Louisiana 9.1%',
-    '41': 'Maine 12.4%',
-    '35': 'Maryland 11.3%',
-    '37': 'Massachusetts 11.5%',
-    '5': 'Michigan 8.6%',
-    '39': 'Minnesota 12.1%',
-    '21': 'Mississippi 9.8%',
-    '13': 'Missouri 9.3%',
-    '27': 'Montana 10.5%',
-    '38': 'Nebraska 11.5%',
-    '18': 'Nevada 9.6%',
-    '16': 'New Hampshire 9.6%',
-    '45': 'New Jersey 13.2%',
-    '25': 'New Mexico 10.2%',
-    '50': 'New York 15.9%',
-    '23': 'North Carolina 9.9%',
-    '7': 'North Dakota 8.8%',
-    '24': 'Ohio 10.0%',
-    '10': 'Oklahoma 9.0%',
-    '31': 'Oregon 10.8%',
-    '28': 'Pennsylvania 10.6%',
-    '36': 'Rhode Island 11.4%',
-    '9': 'South Carolina 8.9%',
-    '4': 'South Dakota 8.4%',
-    '3': 'Tennessee 7.6%',
-    '6': 'Texas 8.6%',
-    '40': 'Utah 12.1%',
-    '47': 'Vermont 13.6%',
-    '43': 'Virginia 12.5%',
-    '30': 'Washington 10.7%',
-    '22': 'West Virginia 9.8%',
-    '32': 'Wisconsin 10.9%',
-    '2': 'Wyoming 7.5%'
-}
+# # Returns political affiliation by state based upon user input
+# def politics(user_input):
 
-while True:
-    state_choice = input('Enter a state name or rank to view the state\'s effective tax burden or \'q\' to continue to the next section: ')
-    if state_choice == 'q':
-        break
-    elif tax_burden(state_choice) == None:
-        print('You have entered an invalid response.')
-    else:
-        print(tax_burden(state_choice))
-    another_state = input('Would you like to view another state? Enter \'y\' for yes or \'n\' for no: ')
-    if another_state == 'y':
-        pass
-    elif another_state == 'n':
-        break
-    else:
-        print('You have entered an invalid response.')
-        continue
+pprint(soup_politics)
+
+
+# # Returns crime rates (violent/property crime) by state based upon user input
+# def crime(user_input):
+#     for
+
+
+# # Returns median home values by stae based upon user input
+# def home_pricing(user_input):
+#     for
+
+
+
+# ##### Will likely remove later, if not used #####
+# state_rank = {
+#     '20': 'Alabama 9.8%', 
+#     '1': 'Alaska 4.6%',
+#     '15': 'Arizona 9.5%',
+#     '26': 'Arkansas 10.2%',
+#     '46': 'California 13.5%',
+#     '19': 'Colorado 9.7%',
+#     '49': 'Connecticut 15.4%',
+#     '42': 'Delaware 12.4%',
+#     '11': 'Florida 9.1%',
+#     '8': 'Georgia 8.9%',
+#     '48': 'Hawaii 14.1%',
+#     '29': 'Idaho 10.7%',
+#     '44': 'Illinois 12.9%',
+#     '14': 'Indiana 9.3%',
+#     '34': 'Iowa 11.2%',
+#     '33': 'Kansas 11.2%',
+#     '17': 'Kentucky 9.6%',
+#     '12': 'Louisiana 9.1%',
+#     '41': 'Maine 12.4%',
+#     '35': 'Maryland 11.3%',
+#     '37': 'Massachusetts 11.5%',
+#     '5': 'Michigan 8.6%',
+#     '39': 'Minnesota 12.1%',
+#     '21': 'Mississippi 9.8%',
+#     '13': 'Missouri 9.3%',
+#     '27': 'Montana 10.5%',
+#     '38': 'Nebraska 11.5%',
+#     '18': 'Nevada 9.6%',
+#     '16': 'New Hampshire 9.6%',
+#     '45': 'New Jersey 13.2%',
+#     '25': 'New Mexico 10.2%',
+#     '50': 'New York 15.9%',
+#     '23': 'North Carolina 9.9%',
+#     '7': 'North Dakota 8.8%',
+#     '24': 'Ohio 10.0%',
+#     '10': 'Oklahoma 9.0%',
+#     '31': 'Oregon 10.8%',
+#     '28': 'Pennsylvania 10.6%',
+#     '36': 'Rhode Island 11.4%',
+#     '9': 'South Carolina 8.9%',
+#     '4': 'South Dakota 8.4%',
+#     '3': 'Tennessee 7.6%',
+#     '6': 'Texas 8.6%',
+#     '40': 'Utah 12.1%',
+#     '47': 'Vermont 13.6%',
+#     '43': 'Virginia 12.5%',
+#     '30': 'Washington 10.7%',
+#     '22': 'West Virginia 9.8%',
+#     '32': 'Wisconsin 10.9%',
+#     '2': 'Wyoming 7.5%'
+# }
+
+# while True:
+#     state_choice = input('Enter a state name or rank to view the state\'s effective tax burden or \'q\' to continue to the next section: ')
+#     if state_choice == 'q':
+#         break
+#     elif tax_burden(state_choice) == None:
+#         print('You have entered an invalid response.')
+#     else:
+#         print(tax_burden(state_choice))
+#     another_state = input('Would you like to view another state? Enter \'y\' for yes or \'n\' for no: ')
+#     if another_state == 'y':
+#         pass
+#     elif another_state == 'n':
+#         break
+#     else:
+#         print('You have entered an invalid response.')
+#         continue
