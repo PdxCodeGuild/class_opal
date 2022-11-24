e3099a12f198a1066afd9c954eefdf796e067c85
lizzie <elizabeth.j.quinlan@gmail.com>
2022-11-23 16:34:43 -0800

    Lizzie progress mini capstone day 1

--- code/lizzie/python/mini_capstone/mini_capstone.py
+++ code/lizzie/python/mini_capstone/mini_capstone.py
@@ -1,12 +1,6 @@
 """ 
 mini capstone: Pokémon Face-Off 
 """
-# User gives stats about their pokemon.
-# How do we resolve not knowing what the enemy's info is?
-
-# All we really need is user's pokemon name
-# Make call to API to return info about that Pokemon
-# Take that info and decide who the winner is with it.
 
 import requests
 from pprint import pprint
@@ -21,98 +15,108 @@ def retrieve_type(user_input):
     url = f'https://pokeapi.co/api/v2/pokemon/{user_input}/'
 
     response = requests.get(url)
-    response_text = response.json()
-    # Some pokemon have 2 types, so this loop iterates through
-    # both slots and appends them to the list as strings.
-    user_type = []
-    for slot in response_text['types']:
-        user_type.append(slot['type']['name'])
-    return user_type
+    try:
+        response_text = response.json()
+        user_type = []
+        for slot in response_text['types']:
+            user_type.append(slot['type']['name'])
+        return user_type
+    except json.decoder.JSONDecodeError:
+        # Trying to handle making a new pokemon with a type that's
+        # Found in the list.
+        # chosen_type = str(input('Please provide a type: '))
+        # if chosen_type in response_text:
+        #     return [chosen_type]
+        # else:
+        print(f"New Pokemon created. {user_input} is an Electric type.")
+        return ['electric']
 
 
-#TODO: Do I need to keep this? How useful is this going to be for me?
-def pokemon_signup():
-    '''
-    Takes the input of a user's pokemon name and returns the types of that user's Pokémon.
-    '''
-    pokename1 = str(input("Please enter your Pokémon's name: ").lower())
-    # user 1 is being assigned the list of types returned from retrieve_type,
-    # which takes pokename1 as its parameter.
-    user1_type: list = retrieve_type(pokename1)
-    print(f"User 1's {pokename1} is a(n) {user1_type} type.")
-    pokename2 = str(input("Please enter your Pokémon's name: ").lower())
-    user2_type: list = retrieve_type(pokename2)
-    print(f"User 2's {pokename2} is a(n) {user2_type} type.")
-    return user1_type, user2_type
+while True:
+    # user is being assigned the list of types returned from retrieve_type,
+    # which takes pokename as its parameter.
+    pokename = str(input("Please enter your Pokémon: ").lower())
+    pokerival = str(input("Please enter your rival's Pokémon: ").lower())
+    user_types = [retrieve_type(pokename), retrieve_type(pokerival)]
+    user_types = [user_types, pokename, pokerival]
+    break
 
 
-# pokemon_signup()
+def find_type_relation(user_types: list):
+        '''
+        Uses pokeapi to determine the relationship of the given type to all other types.
+        Returns a dictionary of four lists with the following keys: 'half_damage_to', 
+        'no_damage_to', 'double_damage_to', and 'quad_damage_to'.
+        '''
+        relationsdict = {
+            "half_damage_to": [],
+            "no_damage_to": [],
+            "double_damage_to": [],
+            "quad_damage_to": []
+        }
+
+        for user_type in user_types:
+            
+            url = f'https://pokeapi.co/api/v2/type/{user_type}/'
+
+            response = requests.get(url)
+            response_text = response.json()
+
+            for name in response_text['damage_relations']['half_damage_to']:
+                if name['name'] not in relationsdict['half_damage_to']:
+                    relationsdict['half_damage_to'].append(name['name'])
+            for name in response_text['damage_relations']['no_damage_to']:
+                if name['name'] not in relationsdict['no_damage_to']:
+                    relationsdict['no_damage_to'].append(name['name'])
+            for name in response_text['damage_relations']['double_damage_to']:
+                # If it is already in the dictionary, both types stack and deal more damage.
+                if name['name'] in relationsdict["double_damage_to"]:
+                    relationsdict['quad_damage_to'].append(name['name'])
+                if name['name'] not in relationsdict['double_damage_to']:
+                    relationsdict['double_damage_to'].append(name['name'])
+
+        # pprint(relationsdict)
+        return relationsdict
 
 
-#TODO: How do you handle dual-type pokemon, where the list has two things in it?
-# Have to parse the list and feed each one to the function.
-# ANOTHER function to fun this function with each of the user types? Should pokemon_signup return
-# strings instead?
-def find_type_relation(user_type):
-    '''
-    Returns the relationship of the given type to all other types.
-    Only does one type at a time.
-    '''
-    users_type = 'fire'
-    url = f'https://pokeapi.co/api/v2/type/{users_type}/'
+def game(user1_type, user2_type):
+    # user_types: list = (pokemon_signup()[0])
+    relationsdict1 = find_type_relation(user1_type)
+    pprint(relationsdict1)
+    relationsdict2 = find_type_relation(user2_type)
+    # print(user2_type)
+    # pprint(relationsdict1['double_damage_to'])
 
-    response = requests.get(url)
-    response_text = response.json()
-    # pprint(response_text)
-
-    pprint(response_text['damage_relations'])
-    ...
-
-users_type = 'fire'
-url = f'https://pokeapi.co/api/v2/type/{users_type}/'
-
-response = requests.get(url)
-response_text = response.json()
-# pprint(response_text)
-
-# pprint(response_text['damage_relations']['double_damage_to'])
-# pprint(response_text['damage_relations'])
+    # Deciding if the rival's type is weak to the user's pokemon.
+    #If re-using, might use and call from a function 
+    def damage_calculator(user_type, relationsdict):
+        '''
+        If I decide to make the following 'for' loop into a function:
+        '''
+        ...
+    for type in user2_type:
+        if type in relationsdict1["double_damage_to"]:
+            print(f"{users[1]} deals double damage.")
+        if type in relationsdict1["half_damage_to"]:
+            print(f"{users[1]} deals half damage.")
+        if type in relationsdict1["no_damage_to"]:
+            print(f"{users[1]} deals no damage.")
+        if type in relationsdict1["quad_damage_to"]:
+            print(f"{users[1]} deals 4x damage.")
+        else: print(f"{users[1]} dealt usual damage.")
 
 
-
-pprint(response_text['damage_relations'])
-print( '!' * 8)
-pprint(response_text['damage_relations']['half_damage_to'])
-print( '!' * 8)
-pprint(response_text['damage_relations']['half_damage_to'][0])
-print( '!' * 8)
-pprint(response_text['damage_relations']['half_damage_to'][0]['name'])
-
-
-# I went one too deep with all of this?
-half_damage_to = []
-
-for name in response_text['damage_relations']['half_damage_to']:
-    half_damage_to.append([name][0]['name'])
-    print(half_damage_to)
+    # relationsdict2 = find_type_relation(user_type_2)
+    # if user1_type in 
+    # pprint(user_type_1, relationsdict1)
+    # pprint(user_type_1, relationsdict2)
+    # pprint(user_types)
+    # pprint(relationsdict1)
+    return relationsdict1
 
 
-    # type_relation.append(damage[0])
-    # type_relation.append(damage)
-    # type_relation.append(response_text) 
+users: list = user_types
+user1_type = users[0][0]
+user2_type = users[0][1]
 
-""" #  for appending "double_damage_from", etc.
-    for name in response_text['damage_relations']:
-        type_relation.append(name)
-        print(type_relation) """
-
-# find_type_relation('steel')
-
-#make another API call to retrieve what the heck happens
-# with these two dudes.
-
-# Pokemon (endpoint): retieving type
-# Types (endpoint) will be a differnt API call.
-    # TypeRelations (type)
-
-#Gathering the two types
+game(user1_type, user2_type)
