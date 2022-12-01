5d71d4b401a1e75e5005d1bb3d33757f630fefa3
Danny Burrow <danny@pdxcodeguild.com>
2022-12-01 14:41:10 -0800

    add advent of code folder and day 1 solutions

---
+++ advent-of-code/danny/day1.py
@@ -0,0 +1,24 @@
+def find_calorie_counts_per_elf(calories: str):
+    calorie_lists: list = calories.split('\n\n')
+    for i in range(len(calorie_lists)):
+        calorie_lists[i] = sum(
+            [int(c) for c in calorie_lists[i].split('\n')])
+    return calorie_lists
+
+
+def find_max_calories(calories: str):
+    calorie_totals = find_calorie_counts_per_elf(calories)
+    return max(calorie_totals)
+
+
+def find_top_3_total(calories: str):
+    calorie_lists: list = find_calorie_counts_per_elf(calories)
+    calorie_lists.sort()
+    return sum(calorie_lists[-3:])
+
+
+if __name__ == '__main__':
+    with open('advent-of-code/puzzles/day1/input1.txt') as f:
+        calories: str = f.read()
+    print(find_max_calories(calories))
+    print(find_top_3_total(calories))
