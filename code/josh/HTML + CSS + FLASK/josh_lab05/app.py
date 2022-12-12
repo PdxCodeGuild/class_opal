b8341a0d061998f65e1e70a83f11b8dc3362e810
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-12 15:36:09 -0800

    add josh html lab 05 progress to code

--- code/josh/HTML + CSS + FLASK/josh_lab05/app.py
+++ code/josh/HTML + CSS + FLASK/josh_lab05/app.py
@@ -1,9 +1,13 @@
 # Lab 05: Redo Python Lab (Unit Converter) as Flask App #User enters distance, input & output units, and app shows them conversion(m).
 
-from flask import Flask, render_template
-import unit_converter.py
+from flask import Flask, render_template, request, redirect
+import unit_converter
 
 app = Flask(__name__)
 
+@app.route('/')
 def index():
+    return render_template(index.html)
     ...
+
+app.run(debug=True)
