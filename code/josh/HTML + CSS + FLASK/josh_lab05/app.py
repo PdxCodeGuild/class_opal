40724e301c4d5b597f358e08646a8516674445c5
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-13 14:59:56 -0800

    add josh html lab 05 progress to code

--- code/josh/HTML + CSS + FLASK/josh_lab05/app.py
+++ code/josh/HTML + CSS + FLASK/josh_lab05/app.py
@@ -1,18 +1,26 @@
 # Lab 05: Redo Python Lab (Unit Converter) as Flask App - User enters distance, input & output units, and app shows them conversion(m).
 
 from flask import Flask, render_template, request, redirect
-from unit_converter import unit_converter
+from unit_converter import unit_converter as converter
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html, context=converter)
+    return render_template('index.html', context=converter)
 
 
-@app.route('/submit-converter/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def handle_submit():
-    unit_converter(request.form)
+    if request.method == 'POST':
+        user_data = {
+        'user_distance': request.form['user_distance'],
+        'units_input': request.form['units_input'],
+        'units_output': request.form['units_output']
+        }
+        unit_converter = converter(user_data['user_distance'], user_data['units_input'], user_data['units_output'])
 
+    return render_template('index.html', context=unit_converter)
+   
 
 app.run(debug=True)
--- code/josh/HTML + CSS + FLASK/josh_lab05/unit_converter.py
+++ code/josh/HTML + CSS + FLASK/josh_lab05/unit_converter.py
@@ -1,12 +1,12 @@
 #Unit Converter
 
-def unit_converter():
-    user_distance = int(input("What is the distance (ft, mi, m, km, yd or in)? "))
-    units_input = input("What are the input units (ft, mi, m, km, yd, or in)? ")
-    units_output = input("What are the output units (ft, mi, m, km, yd, or in)? ")
+def unit_converter(user_distance, units_input, units_output):
+    user_distance = int(user_distance)
     conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
     new_distance = user_distance * conversion[units_input]
     new_units = new_distance / conversion[units_output]
-    print(f"{user_distance} {units_input} is {new_units} {units_output}.")
-
-unit_converter()
+    return {'user_distance': user_distance,
+            'units_input': units_input,
+            'new_distance': new_distance,
+            'new_units': new_units
+    }
