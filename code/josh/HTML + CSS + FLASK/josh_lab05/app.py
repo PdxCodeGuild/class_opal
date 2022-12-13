09493314d4889c6bcf7c3440f9d312dbf7d427bb
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-13 12:31:39 -0800

    add josh html lab 05 progress to code

--- code/josh/HTML + CSS + FLASK/josh_lab05/app.py
+++ code/josh/HTML + CSS + FLASK/josh_lab05/app.py
@@ -1,13 +1,18 @@
-# Lab 05: Redo Python Lab (Unit Converter) as Flask App #User enters distance, input & output units, and app shows them conversion(m).
+# Lab 05: Redo Python Lab (Unit Converter) as Flask App - User enters distance, input & output units, and app shows them conversion(m).
 
 from flask import Flask, render_template, request, redirect
-import unit_converter
+from unit_converter import unit_converter
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
-    ...
+    return render_template(index.html, context=converter)
+
+
+@app.route('/submit-converter/', methods=['POST'])
+def handle_submit():
+    unit_converter(request.form)
+
 
 app.run(debug=True)
--- code/josh/HTML + CSS + FLASK/josh_lab05/unit_converter.py
+++ code/josh/HTML + CSS + FLASK/josh_lab05/unit_converter.py
@@ -1,20 +1,12 @@
-# Lab 1 - Unit Converter, Version 4
+#Unit Converter
 
-# Creates a variable that stores user input of a distance as units
-user_distance = input("What is the distance (ft, mi, m, km, yd or in)? ")
-# Typecasts user input to an integer for conversion
-user_distance = int(user_distance)
-# Creates a variable that stores user input of a starting unit of measure
-units_input = input("What are the input units (ft, mi, m, km, yd, or in)? ")
-# Creates a variable that stores the user's desired unit of measure as an output 
-units_output = input("What are the output units (ft, mi, m, km, yd, or in)? ")
-# Creates a dictionary that stores units of measure as keys and numeric conversions as values
-conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
-# Creates a variable that converts user input of distance to meters, according to user input of
-# desired starting units
-new_distance = user_distance * conversion[units_input]
-# Creates a variable that converts meter conversion to user input of desired output units 
-new_units = new_distance / conversion[units_output]
-# Displays the conversion of user's inputs of distance and unit measure to user's desired unit 
-# measureoutput
-print(f"{user_distance} {units_input} is {new_units} {units_output}.")
+def unit_converter():
+    user_distance = int(input("What is the distance (ft, mi, m, km, yd or in)? "))
+    units_input = input("What are the input units (ft, mi, m, km, yd, or in)? ")
+    units_output = input("What are the output units (ft, mi, m, km, yd, or in)? ")
+    conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
+    new_distance = user_distance * conversion[units_input]
+    new_units = new_distance / conversion[units_output]
+    print(f"{user_distance} {units_input} is {new_units} {units_output}.")
+
+unit_converter()
