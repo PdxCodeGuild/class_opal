a00aa9efffa1f8a17a4fa5fffcc5107aa582f9be
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-12-14 15:10:17 -0800

    rachel html lab 5 completed

--- code/rachel/HTML/HTML_Lab_5/cypher_test.py
+++ code/rachel/HTML/HTML_Lab_5/cypher_test.py
@@ -1,12 +1,7 @@
-s = input("What letters or word do you want to encrypt? ")
+abc = "abcdefghijklmnopqrstuvwxyz"
+word = input("What letters or word do you want to encrypt? ")
+n = int(input("Enter the cipher rotation amount (1-25): "))
 
-n = input("Enter the cipher rotation amount (1-25): ")
+rot = lambda x: "".join([abc[(abc.find(c) + n) % 26] for c in x])
 
-n = int(n)
-
-def rot_alpha(n):
-    from string import ascii_lowercase as lc, ascii_uppercase as uc
-    lookup = str.maketrans(lc + uc, lc[n:] + lc[:n] + uc[n:] + uc[:n])
-    return lambda s: s.translate(lookup)
-
-print(rot_alpha(n))
+print(rot(word))
