1b00b4a4731a40f2b1ca97ab9ae29ba6290f7658
lizzie <elizabeth.j.quinlan@gmail.com>
2022-12-21 10:25:49 -0800

    update views.py index

--- code/lizzie/django/mysite/grocery_list/urls.py
+++ code/lizzie/django/mysite/grocery_list/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 # Need to map view to URLconf to handle it.
 urlpatterns = [
+    # ex: /grocery_item
     path('', views.index, name='index'),
+    # ex: /grocery_item/5/
+    path('<int:text_desc_id>/', views.detail, name='detail')
 ]
--- code/lizzie/django/mysite/grocery_list/views.py
+++ code/lizzie/django/mysite/grocery_list/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import HttpResponse
 
+from .models import GroceryItem
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hellow, world. You're at the grocery list.")
+    latest_grocery_items = GroceryItem.objects.order_by('created_date')[:5]
+    output = ','.join([i.text_desc for i in latest_grocery_items])
+    return HttpResponse(output)
+
+def detail(request, text_desc_id):
+    return HttpResponse(f"You're looking at item {text_desc_id}.")
