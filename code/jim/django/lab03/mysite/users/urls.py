36193cec0c39852a28e9edcff607858e665ae7a1
jbrennan <jamesbrennan21@gmail.com>
2022-12-21 10:43:51 -0800

    added users signup

--- code/jim/django/lab03/mysite/users/urls.py
+++ code/jim/django/lab03/mysite/users/urls.py
@@ -1,7 +1,9 @@
+# accounts/urls.py
 from django.urls import path
 
-from . import views
+from .views import SignUpView
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
