f59ba8e35a8c866da2acb9518955c9bd03381a67
lizzie <elizabeth.j.quinlan@gmail.com>
2022-12-21 10:47:43 -0800

    create detail.html template

--- code/lizzie/django/mysite/grocery_list/views.py
+++ code/lizzie/django/mysite/grocery_list/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import GroceryItem
 
@@ -6,8 +7,17 @@ from .models import GroceryItem
 # Create your views here.
 def index(request):
     latest_grocery_items = GroceryItem.objects.order_by('created_date')[:5]
-    output = ','.join([i.text_desc for i in latest_grocery_items])
-    return HttpResponse(output)
+    context = {
+        'latest_grocery_items': latest_grocery_items,
+    }
+    # The render() function takes the request object as its first argument, a template name as its 
+    # second argument and a dictionary as its optional third argument. It returns an HttpResponse 
+    # object of the given template rendered with the given context.
+    return render(request, 'grocery_list/index.html', context)
 
 def detail(request, text_desc_id):
-    return HttpResponse(f"You're looking at item {text_desc_id}.")
+    try:
+        text_desc = GroceryItem.objects.get(pk=text_desc_id)
+    except GroceryItem.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'grocery_list/detail.html', {'text_desc': text_desc})
