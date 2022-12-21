ebb677f4deb09483ccca5b3136403f5d06d43ee1
lizzie <elizabeth.j.quinlan@gmail.com>
2022-12-20 16:29:11 -0800

    create GroceryItem model

--- code/lizzie/django/mysite/grocery_list/models.py
+++ code/lizzie/django/mysite/grocery_list/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Here, each model is represented by a class that subclasses django.db.models.Model. 
+# Each model has a number of class variables, each of which represents a database 
+# field in the model.
+class GroceryItem(models.Model):
+    text_desc = models.CharField(max_length=200)
+    created_date = models.DateTimeField('create date')
+    completed_date = models.DateTimeField('complete date')
+    # I'll figure out how to fix this later!!! Don't get mad at me program!
+    is_complete = models.BooleanField(null=False)
