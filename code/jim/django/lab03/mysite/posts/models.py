df01117181fba99d7eb82bbdc6b462f061549ea6
jbrennan <jamesbrennan21@gmail.com>
2022-12-21 16:28:36 -0800

    added posts model and migrated to db

--- code/jim/django/lab03/mysite/posts/models.py
+++ code/jim/django/lab03/mysite/posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_text = models.CharField(max_length=280)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
