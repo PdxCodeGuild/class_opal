56eedb6c496aad1be223ad939917210f6749b2fb
jbrennan <jamesbrennan21@gmail.com>
2022-12-22 15:46:58 -0800

    added navbar and public posts

--- code/jim/django/lab03/mysite/posts/urls.py
+++ code/jim/django/lab03/mysite/posts/urls.py
@@ -6,4 +6,5 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_post/', views.add_post, name='add_post'),
+    path('<int:user_id>/', views.user_posts, name='user_posts'),
 ]
--- code/jim/django/lab03/mysite/posts/views.py
+++ code/jim/django/lab03/mysite/posts/views.py
@@ -2,16 +2,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, User
 
 
 def index(request):
-    post_list = Post.objects.all()
-    context = {'posts': post_list}
+    posts = Post.objects.all()
+    user = request.user
+    filtered_posts = posts.filter(user_id=user.id)
+    context = {'posts': filtered_posts}
     return render(request, 'home.html', context)
 
 
 def add_post(request):
     post_text = request.POST['new_post']
-    new_post = Post.objects.create(user=request.user, post_text=post_text)
+    Post.objects.create(user=request.user, post_text=post_text)
     return HttpResponseRedirect(reverse('posts:index'))
+
+
+def user_posts(request, user_id):
+    user = User.objects.get(id=user_id)
+    return render(request, 'user-posts.html', {'user': user})
