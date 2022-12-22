0acf83561effc1b261cf95f3b679cd139bb2d745
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-21 16:26:54 -0800

    add josh django lab 01 progress to code

--- code/josh/Django/josh_lab01/grocery_list/models.py
+++ code/josh/Django/josh_lab01/grocery_list/models.py
@@ -1,17 +1,11 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
 
 
 class GroceryItem(models.Model):
     text_description = models.CharField(max_length=200)
-    created_date = models.DateTimeField('created date')
+    created_date = models.DateTimeField('created date', auto_now_add=True)
     completed_date = models.DateTimeField('completed date')
     completed = models.BooleanField()
 
     def __str__(self):
         return self.grocery_list
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- code/josh/Django/josh_lab01/grocery_list/views.py
+++ code/josh/Django/josh_lab01/grocery_list/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from . models import GroceryItem
 
 
 def index(request):
     return HttpResponse("Hello World! You're at the grocery_list index.")
-
-
