1026970ffee434da7ea0fdc4dde30811536bef42
lizzie <elizabeth.j.quinlan@gmail.com>
2022-12-27 14:20:31 -0800

    update displaying urls

--- code/lizzie/django/lab02/mysite/urlshortener/urls.py
+++ code/lizzie/django/lab02/mysite/urlshortener/urls.py
@@ -6,6 +6,6 @@ app_name = 'urlshortener'
 urlpatterns = [
     path('', views.index, name='index'),
     path('submit_url/', views.submit_url, name='submit_url'),
-    # Use string representation of code
+    # Use string representation of code to access link
     path('<str:code>/', views.redirect_user, name='redirect_user'),
 ]
