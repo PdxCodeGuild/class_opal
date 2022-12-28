3f3375797424476f2641e233dbeaa5ab6b742108
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-28 15:26:16 -0800

    created login/logout/signup functionality to project

--- code/josh/Django/josh_lab03/users/urls.py
+++ code/josh/Django/josh_lab03/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
+from .views import SignUpView
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- code/josh/Django/josh_lab03/users/views.py
+++ code/josh/Django/josh_lab03/users/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
-def index(request):  
-    return HttpResponse("Hello, world") 
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
