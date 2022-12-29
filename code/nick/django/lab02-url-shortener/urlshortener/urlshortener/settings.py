01b21742bac7faf06e56e615072ec5a72e170235
NMurdaugh <nmurdaugh42@gmail.com>
2022-12-29 14:18:56 -0600

    url_manager migration

--- code/nick/django/lab02-url-shortener/urlshortener/urlshortener/settings.py
+++ code/nick/django/lab02-url-shortener/urlshortener/urlshortener/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'url_manager.apps.UrlManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
