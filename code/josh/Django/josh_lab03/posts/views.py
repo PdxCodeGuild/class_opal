a74382bcdd5294c20090764e55c37ac2927b65cd
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-29 12:10:19 -0800

    updated posts views

--- code/josh/Django/josh_lab03/posts/views.py
+++ code/josh/Django/josh_lab03/posts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from . models import Posts
 
-def index(request):  
-    return HttpResponse("Hello, world") 
+def index(request):
+    if request.method == 'POST':
+        form_data = request.POST
+        post = Posts.objects.create(
+            chirp = form_data['chirp'], 
+        )
+        return HttpResponseRedirect(reverse('posts:index'))
+    else:
+        post = Posts.objects.all()
+        context = {'posts': post}
+        return render(request, 'posts/index.html', context)
