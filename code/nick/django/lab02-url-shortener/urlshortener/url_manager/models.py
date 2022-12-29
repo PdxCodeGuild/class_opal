44840be2d8408219f32c33a7df8a671ac3d41b3a
NMurdaugh <nmurdaugh42@gmail.com>
2022-12-29 14:14:51 -0600

    url model created

--- code/nick/django/lab02-url-shortener/urlshortener/url_manager/models.py
+++ code/nick/django/lab02-url-shortener/urlshortener/url_manager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from common.url_generator import url_generator
 
 # Create your models here.
+
+
+class Url(models.Model):
+    original_url = models.CharField(max_length=255)
+    short_url = models.CharField(max_length=15, default=url_generator())
