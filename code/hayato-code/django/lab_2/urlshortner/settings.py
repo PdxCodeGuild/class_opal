d689a54dbbffec1ef73d206e540212e0a337b6aa
hayato <hayato.kamohara@gmail.com>
2022-12-29 15:37:12 -0600

    added templates and fixed setting and url

--- code/hayato-code/django/lab_2/urlshortner/settings.py
+++ code/hayato-code/django/lab_2/urlshortner/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # include shorter app in installed apps
+    "shortener"
 ]
 
 MIDDLEWARE = [
--- code/hayato-code/django/lab_2/urlshortner/urls.py
+++ code/hayato-code/django/lab_2/urlshortner/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # include shortner urls
+    path("", include("shortener.urls"))
 ]
