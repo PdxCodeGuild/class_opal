8e4d4a1fd1951269edb810a39dc07e9121303ea3
NMurdaugh <nmurdaugh42@gmail.com>
2022-12-30 17:56:04 -0600

    Post rewritten to add 'rechirp' functionality, Like model created

--- code/nick/django/lab03-chirp/chirp/posts/models.py
+++ code/nick/django/lab03-chirp/chirp/posts/models.py
@@ -8,12 +8,19 @@ class Post(models.Model):
     post_text = models.CharField(max_length=120)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.IntegerField(default=0)
+    original_post = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True)
 
 
 class Comment(models.Model):
     comment_text = models.CharField(max_length=120)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    likes = models.IntegerField(default=0)
+
+
+class Like(models.Model):
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
