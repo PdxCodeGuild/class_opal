abdef663fb92851d5088217f5c4676603637622b
Josh <24633211+josh-on-github@users.noreply.github.com>
2022-12-30 10:08:10 -0800

    josh django lab02 (#201)
    * add josh django lab 01 progress to code
    
    * added superuser and registered model
    
    * completed registering model
    
    * add josh django lab 01 progress to code
    
    * add josh django lab 1 progress to code
    
    * add josh django lab 1 progress to code
    
    * add josh django lab 1 progress to code
    
    * started conditional for complete/incomplete button in html
    
    * finished conditional for complete/incomplete button in html
    
    * added delete item  capability
    
    * created lab 2 project and url shortener app
    
    * made migrations
    
    * created views in progress
    
    * started html and finished index view
    
    * completed url shortener redirect
    
    * add josh django lab 02 final to code
    
    Co-authored-by: Josh <24633211+JFC1@users.noreply.github.com>

---
+++ code/josh/Django/josh_lab02/url_shortener/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from . models import UrlShortener
+
+admin.site.register(UrlShortener)
---
+++ code/josh/Django/josh_lab02/url_shortener/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'url_shortener'
+urlpatterns = [
+    path('redirect/<int:id>', views.redirect, name='redirect'),
+    path('', views.index, name='index'),
+]
