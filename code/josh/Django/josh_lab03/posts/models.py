71e80f925886b91142aa362857f93a91cbdc5fe0
Josh <24633211+JFC1@users.noreply.github.com>
2022-12-30 11:52:33 -0800

    updated Posts model with user and ForeignKey

--- code/josh/Django/josh_lab03/posts/models.py
+++ code/josh/Django/josh_lab03/posts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Posts(models.Model):
     chirp = models.CharField(max_length=128)
-    
+    users = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    pub_date = models.DateTimeField('date published', auto_now_add=True, null=True)
+
     def __str__(self):
         return self.chirp
--- code/josh/Django/josh_lab03/posts/views.py
+++ code/josh/Django/josh_lab03/posts/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . models import Posts
+from django.contrib.auth.models import User
 
 def index(request):
     if request.method == 'POST':
         form_data = request.POST
+        user = User.objects.filter(username=request.user)
         post = Posts.objects.create(
-            chirp = form_data['chirp'], 
+            chirp = form_data['chirp'],
+            users = user[0]
         )
         return HttpResponseRedirect(reverse('posts:index'))
     else:
         post = Posts.objects.all()
+        print('Testing.')
+        print(post)
+        user = User.objects.filter(username=request.user)
+        print(user)
         context = {'posts': post}
         return render(request, 'posts/index.html', context)
