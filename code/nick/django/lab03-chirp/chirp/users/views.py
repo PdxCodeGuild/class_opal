b6c831f37b36107b887a72ada54edf1d074e387a
NMurdaugh <nmurdaugh42@gmail.com>
2022-12-30 18:27:24 -0600

    changed User model to default django auth User

--- code/nick/django/lab03-chirp/chirp/users/views.py
+++ code/nick/django/lab03-chirp/chirp/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from .models import User
+# from .models import User
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -8,4 +9,4 @@ def index(request):
 
 
 def Create(request):
-    ...
+    new_user = User.objects.create_user(...)
