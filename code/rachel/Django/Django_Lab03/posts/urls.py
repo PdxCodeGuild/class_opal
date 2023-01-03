701eb79365ba27925bae637b982b0300e49e4f5c
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2023-01-02 22:34:23 -0800

    Post app wrap-up: added more templates and updated views.py and urls.py

--- code/rachel/Django/Django_Lab03/posts/urls.py
+++ code/rachel/Django/Django_Lab03/posts/urls.py
@@ -7,4 +7,7 @@ app_name = "posts"
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'), #.as_view & camelCase = specific to CBVs
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='detail'), #this view requires one variable; pk is used to only show details for the post w/ the right key
+    path('post/new/', views.BlogCreateView.as_view(), name='new'),
+    path('post/<int:pk>/edit/', views.BlogEditView.as_view(), name='edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='delete'),
 ]
--- code/rachel/Django/Django_Lab03/posts/views.py
+++ code/rachel/Django/Django_Lab03/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy #keeps the server from going to reverse right away before importing the URLs in the classes - reverse_lazy won’t evaluate until the value is needed
 from .models import Post
 
 class BlogListView(ListView): #general read view
@@ -12,15 +12,18 @@ class BlogDetailView(DetailView): #this is the read view for a specific post
     model = Post
     template = 'post_detail.html'
 
-class BlogCreateView(CreateView):
+#create, update & delete views have auto-generated views, just specify which fields you want (dates are added automatically) 
+class BlogCreateView(CreateView): #handles both POST & GET
     model = Post
-    fields = ['name', 'title', 'post']
+    template = 'post_new.html'
+    fields = ['title', 'author', 'body'] #fields are shown on page according to how they are ordered here
 
-class BlogUpdateView(UpdateView):
+class BlogEditView(UpdateView):
     model = Post
-    fields = ['name', 'title', 'post']
-    template = 'edit.html'
+    fields = ['title', 'body'] #the post is already tied to the user name so no need to add the author field
+    template = 'post_edit.html'
 
 class BlogDeleteView(DeleteView):
     model = Post
-    success_url = reverse_lazy('author-list')   
+    success_url = reverse_lazy('posts:home')
+    template = 'post_delete.html'   
