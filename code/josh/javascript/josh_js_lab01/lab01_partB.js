cd6abcfeeafdba0a354aaa17f175e5d3130fb5c0
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-04 14:45:45 -0800

    added unit converter function to lab 1b

--- code/josh/javascript/josh_js_lab01/lab01_partB.js
+++ code/josh/javascript/josh_js_lab01/lab01_partB.js
@@ -4,7 +4,10 @@ let userDistance = document.getElementById('user distance')
 userDistance = parseInt(userDistance)
 const unitsInput = document.getElementById('units input')
 const unitsOutput = document.getElementById('units output')
-conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
+const conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
 const newDistance = userDistance * conversion[unitsInput]
+console.log(newDistance)
 const newUnits = newDistance / conversion[unitsOutput]
-// alert(`${userDistance} ${unitsInput} is ${newUnits} ${unitsOutput}.`)
+function convert() {
+    document.getElementById("converted units").innerHTML = `${userDistance} ${unitsInput} is ${newUnits} ${unitsOutput}.`
+}
