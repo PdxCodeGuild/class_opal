70bdd2c771067fd2371c13f1f9d4f1c3320aa5c6
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-04 15:03:44 -0800

    added event listener to js

--- code/josh/javascript/josh_js_lab01/lab01_partB.js
+++ code/josh/javascript/josh_js_lab01/lab01_partB.js
@@ -1,13 +1,14 @@
 // Unit Converter Part B
-
-let userDistance = document.getElementById('user distance')
-userDistance = parseInt(userDistance)
-const unitsInput = document.getElementById('units input')
-const unitsOutput = document.getElementById('units output')
-const conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
-const newDistance = userDistance * conversion[unitsInput]
-console.log(newDistance)
-const newUnits = newDistance / conversion[unitsOutput]
+let converter = document.getElementById('converter')
+converter.addEventListener('click', convert)
 function convert() {
-    document.getElementById("converted units").innerHTML = `${userDistance} ${unitsInput} is ${newUnits} ${unitsOutput}.`
+    let userDistance = document.getElementById('user_distance').value
+    userDistance = parseInt(userDistance)
+    const unitsInput = document.getElementById('units_input').value
+    console.log(unitsInput)
+    const unitsOutput = document.getElementById('units_output').value
+    const conversion = {"ft": 0.3048, "mi": 1609.34, "m": 1, "km": 1000, "yd": 0.9144, "in": 0.0254}
+    const newDistance = userDistance * conversion[unitsInput]
+    const newUnits = newDistance / conversion[unitsOutput]
+    document.getElementById("converted_units").innerText = `${userDistance} ${unitsInput} is ${newUnits} ${unitsOutput}.`
 }
