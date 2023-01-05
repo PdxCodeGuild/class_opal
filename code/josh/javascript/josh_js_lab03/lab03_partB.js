b3f07cfb40497a786c8eaceb7ec999b037224726
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-05 13:52:53 -0800

    changed while loop in function to conditional only, added a new function, added event listeners

--- code/josh/javascript/josh_js_lab03/lab03_partB.js
+++ code/josh/javascript/josh_js_lab03/lab03_partB.js
@@ -1,18 +1,26 @@
 // Python to JS Lab 3 Part B
 
-let average = document.getElementById('average')
+const average = document.getElementById('average')
 average.addEventListener('click', findAverage)
+const reset = document.getElementById('reset')
+reset.addEventListener('click', resetNumArr)
+let numArr = []
+
+
 function findAverage() {
-    let number = document.getElementById('number')
-    let numArr = []
-    while (true) {
-        if (number == 'done') {
-            document.getElementById("output").innerText = `The average is: ${average}.`
-            break
-        }
-        else {
-            number = parseInt(number)
-            numArr.push(number)
-            let numRedArr = numArr.reduce((x, y) => x + y)
-            average = numRedArr/numArr.length
-        }}}
+    let number = document.getElementById('number').value
+    if (number == 'done') {
+        return document.getElementById("output").innerText = `The average is: ${average}. Click reset to start over.`
+    }
+    else {
+        number = parseInt(number)
+        numArr.push(number)
+        let numRedArr = numArr.reduce((x, y) => x + y)
+        average = numRedArr/numArr.length
+        return document.getElementById("output").innerText = `The average is: ${average}.  Enter another number to continue averaging or 'done' to quit.`
+    }}
+
+    
+function resetNumArr() {
+    numArr = []
+}
