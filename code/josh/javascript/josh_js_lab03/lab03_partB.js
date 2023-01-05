12b4100a75bba4bb49a2c7098a0f0659d151ec55
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-04 20:40:36 -0800

    created body of lab3b html

--- code/josh/javascript/josh_js_lab03/lab03_partB.js
+++ code/josh/javascript/josh_js_lab03/lab03_partB.js
@@ -1,16 +1,19 @@
-// Python Lab 3 Version 2
+// Python to JS Lab 3 Part B
 
-let numArr = []
-
-while (true) {
-    let userInput = prompt("Enter number or 'done' to quit: ")
-    if (userInput == 'done') {
-        alert(`The average is: ${average}`)
-        break
-    }
-    else {
-        userInput = parseInt(userInput)
-        numArr.push(userInput)
-        let numRedArr = numArr.reduce((x, y) => x + y)
-        average = numRedArr/numArr.length
-    }}
+let average = document.getElementById('average')
+average.addEventListener('click', findAverage)
+function findAverage() {
+    let number = document.getElementById('number').value
+    let numArr = []
+    while (true) {
+        let userInput = prompt("Enter number or 'done' to quit: ")
+        if (userInput == 'done') {
+            alert(`The average is: ${average}`)
+            break
+        }
+        else {
+            userInput = parseInt(userInput)
+            numArr.push(userInput)
+            let numRedArr = numArr.reduce((x, y) => x + y)
+            average = numRedArr/numArr.length
+        }}}
