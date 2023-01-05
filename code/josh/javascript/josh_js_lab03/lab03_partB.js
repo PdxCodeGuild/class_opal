3cc07b8817964136c6e60c7f5b322b6b38a11e7b
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-05 14:35:44 -0800

    added text field value clearing with click events and reset button with functionality

--- code/josh/javascript/josh_js_lab03/lab03_partB.js
+++ code/josh/javascript/josh_js_lab03/lab03_partB.js
@@ -1,26 +1,33 @@
 // Python to JS Lab 3 Part B
 
-const average = document.getElementById('average')
+let average = document.getElementById('average')
 average.addEventListener('click', findAverage)
-const reset = document.getElementById('reset')
+let reset = document.getElementById('reset')
 reset.addEventListener('click', resetNumArr)
 let numArr = []
-
 
 function findAverage() {
     let number = document.getElementById('number').value
     if (number == 'done') {
+        document.getElementById('number').value = ''
         return document.getElementById("output").innerText = `The average is: ${average}. Click reset to start over.`
+    }
+    else if (isNaN(number)) {
+        document.getElementById('number').value = ''
+        document.getElementById('output').innerText = 'You didn\'t make a valid entry.  Click reset and try again.'
     }
     else {
         number = parseInt(number)
         numArr.push(number)
         let numRedArr = numArr.reduce((x, y) => x + y)
         average = numRedArr/numArr.length
+        document.getElementById('number').value = ''
         return document.getElementById("output").innerText = `The average is: ${average}.  Enter another number to continue averaging or 'done' to quit.`
     }}
-
     
 function resetNumArr() {
     numArr = []
+    document.getElementById('number').value = ''
+    document.getElementById('output').innerText = ''
+
 }
