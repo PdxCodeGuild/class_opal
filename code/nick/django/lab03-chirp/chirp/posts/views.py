ae6f5406de112221365889da7b5ade198fb2e2dc
NMurdaugh <nmurdaugh42@gmail.com>
2023-01-05 11:31:10 -0700

    takes user to login page if not logged in

--- code/nick/django/lab03-chirp/chirp/posts/views.py
+++ code/nick/django/lab03-chirp/chirp/posts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Comment, Like, Post
 # Create your views here.
 
 
 def index(request):
-    post_feed = Post.objects.all().order_by('-pub_date')
-    return render(request, 'posts/index.html', {'post_feed': post_feed})
+    user = request.user
+    if user.is_anonymous:
+        return HttpResponseRedirect('registration/login.html')
+    else:
+        post_feed = Post.objects.all().order_by('-pub_date')
+        return render(request, 'posts/index.html', {'post_feed': post_feed})
 
 
 def like(request, post_id):
