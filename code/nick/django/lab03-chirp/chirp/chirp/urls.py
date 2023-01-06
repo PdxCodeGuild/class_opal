f6e3c8e1c8bf7937144f732d2d2250ccfe18ee69
NMurdaugh <nmurdaugh42@gmail.com>
2023-01-06 10:38:23 -0700

    broken. user creation page not loading properly. Committing to work on other project

--- code/nick/django/lab03-chirp/chirp/chirp/urls.py
+++ code/nick/django/lab03-chirp/chirp/chirp/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 urlpatterns = [
+    path('home/', lambda request: render(request, 'home.html')),
     path('', include('posts.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
--- code/nick/django/lab03-chirp/chirp/chirp_users/views.py
+++ code/nick/django/lab03-chirp/chirp/chirp_users/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
-def Create(request):
-    ...
+def index(request):
+    return render(request, 'chirp_users/index.html')
+
+
+def create(request):
+    new_user = User.objects.create_user(
+        request.POST['username'], ..., request.POST['password'])
+    return HttpResponseRedirect('')
