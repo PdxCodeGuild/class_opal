8e5ab9d133e434a559b8a13e98b4adc13842c426
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-06 16:33:25 -0800

    js_mob updates 3rd driver

--- code/mobs/JS_Mob/todo.js
+++ code/mobs/JS_Mob/todo.js
@@ -1,53 +1,37 @@
-
-
-
-// select all of the elements we need to change and/or listen to
 const new_item = document.getElementById('new_item').value
 const myButton = document.querySelector('#button')
-// const checkboxes = document.querySelectorAll('input[type=checkbox]')
-
-// const completed_list = document.create
-
 const completedList = []
-const incompleteList = ['check that code works', 'a', 'b', 'check for back ticks']
+const incompleteList = []
 
-// check whether the button should be disabled or not
-// const checkValidity = () => {
-//     const checked = Array.from(checkboxes).filter(el => el.checked)
-//     if (checked.length > 0 && voter.value) {
-//         myButton.removeAttribute('disabled')
-//     } else {
-//         myButton.setAttribute('disabled', true)
-//     }
-// }
+function incompleteListFunction () {
+    for(item of incompleteList) {
+        const newDiv = document.createElement('div')
+        const container = document.querySelector('#container')
+        const checkbox = document.createElement('input')
+        checkbox.type='checkbox'
+        checkbox.id=`checkbox${incompleteList.indexOf(item)}`
+        const input = document.createTextNode(item)
+        const deleteButton = document.createElement('button')
+        deleteButton.id=`delete-${incompleteList.indexOf(item)}`
+        deleteButton.innerText='delete'
+        newDiv.appendChild(checkbox)
+        newDiv.appendChild(input)
+        newDiv.appendChild(deleteButton)
+        container.appendChild(newDiv)
+}}
 
-for(item of incompleteList) {
-    const newDiv = document.createElement('div')
-    const checkbox = document.createElement('input')
-    checkbox.type='checkbox'
-    checkbox.id=`checkbox${incompleteList.indexOf(item)}`
-    const input = document.createTextNode(item)
-    const deleteButton = document.createElement('button')
-    deleteButton.id=`delete${incompleteList.indexOf(item)}`
-    deleteButton.innerText='delete'
-    newDiv.appendChild(checkbox)
-    newDiv.appendChild(input)
-    newDiv.appendChild(deleteButton)
-    const literallyAnything = document.getElementById('new_todo')
-    document.body.after(newDiv, literallyAnything)
-
-    // create element div
-    // create element checkbox
-    // create element input
-    // create element button with innertext delete
-    // append child to go inside div
-    // publish in line
-}
-
+document.body.onload = incompleteListFunction()
 myButton.addEventListener('click', () => {
-    incompleteList.push(querySelector("#newItem").value)
+    incompleteList.push(document.getElementById("new_item").value)
+    while (document.getElementById('container').firstElementChild) {
+        document.getElementById('container').firstElementChild.remove()
+    }
+    incompleteListFunction()
+})
 
-    // Take input and push to the events
-    alert('Here!')
-    alert(`${incompleteList}`)
+deleteButton.addEventListener('click', () => { 
+const deleteIndex = deleteButton.id.split('-')[1]
+deleteIndex.remove()
+completedList = []
+completedList.appendChild()
 })
