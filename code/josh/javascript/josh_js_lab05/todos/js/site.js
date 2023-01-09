afcf01f5d843ccb2b120c917cae197464cbad480
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-09 14:46:02 -0800

    started building Vue app and html

--- code/josh/javascript/josh_js_lab05/todos/js/site.js
+++ code/josh/javascript/josh_js_lab05/todos/js/site.js
@@ -1,6 +1,23 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello world!'
-    }
-})
+        listItems: ['Laundry'],
+        completed: '',
+        delete: ''
+    },
+    userItem: () => {
+        this.listItems.push()
+    },
+    methods: {
+        logInput(e) {
+            this.userItem = e.target.value
+        }
+    }})
+
+
+/* 
+Store an array of objects (the todos themselves)
+List each todo
+Allow the user to add and remove todos
+Allow a user to toggle if a task is complete or not
+*/
