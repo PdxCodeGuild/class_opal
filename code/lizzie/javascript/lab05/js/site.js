cdf2cdbf374ef067fa4b43859a09a48c403cc9db
lizzie <elizabeth.j.quinlan@gmail.com>
2023-01-09 14:40:57 -0800

    create functionality for adding items

--- code/lizzie/javascript/lab05/js/site.js
+++ code/lizzie/javascript/lab05/js/site.js
@@ -1,6 +1,22 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello world!'
+        cohortName: 'Class Opal',
+        startDate: 'October 31',
+        message: 'Hello world!',
+        newTodoText: '',
+        todos: [
+        {item: 'Do the dishes'},
+        ],
+        completeTodo: [''],
+        inputField: 'starter value',
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push({
+              item: this.newTodoText
+            })
+            this.newTodoText = ''
+          }
     }
 })
