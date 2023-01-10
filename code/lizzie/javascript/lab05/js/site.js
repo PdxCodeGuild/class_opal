9b5fc054a24489cf48550de5cfa9138a3ac4662a
lizzie <elizabeth.j.quinlan@gmail.com>
2023-01-10 10:51:34 -0800

    add id value to data

--- code/lizzie/javascript/lab05/js/site.js
+++ code/lizzie/javascript/lab05/js/site.js
@@ -2,34 +2,42 @@ new Vue({
     el: '#app',
     data: {
         newTodoText: '',
-        todos: [],
+        todos: [
+            {item: 'Learn how to make a todo list',
+            completed: false,
+            id: 1,}
+        ],
+        itemIncrement: 1
     },
     methods: {
         addNewTodo: function () {
             this.todos.push({
+                // builds the new todo item then clears the string afterward to be reused
               item: this.newTodoText,
               completed: false,
+              id: this.itemIncrement,
             },)
+            this.itemIncrement++
             this.newTodoText = ''
         },
         removeTodo(todoItem) {
             // Remove item at its index in the array
-            let myIndex = this.todos.indexOf(todoItem)
+            const myIndex = this.todos.indexOf(todoItem)
             this.todos.splice(myIndex, 1);
         },
-        markComplete(index) {
-            this.todos[index].completed = true;
+        markComplete(id) {
+            this.id.completed = true;
         },
-        markIncomplete(index) {
-            this.todos[index].completed = false;
+        markIncomplete(id) {
+            this.todos[id].completed = false;
         },
     },
     computed: {
-        incompleteTodos() {
-            return this.todos.filter(todo => !todo.completed);
-        },
         completeTodos() {
             return this.todos.filter(todo => todo.completed);
+        },
+        incompleteTodos() {
+            return this.todos.filter(todo => !todo.completed);
         },
     },
 },
