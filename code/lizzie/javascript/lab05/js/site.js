a49be70c4d49e0ff251015a3818bad71933aac18
lizzie <elizabeth.j.quinlan@gmail.com>
2023-01-09 17:23:33 -0800

    update to move items to complete and incomplete lists

--- code/lizzie/javascript/lab05/js/site.js
+++ code/lizzie/javascript/lab05/js/site.js
@@ -1,28 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-        cohortName: 'Class Opal',
-        startDate: 'October 31',
-        message: 'Hello world!',
         newTodoText: '',
-        todos: [
-        {item: 'This inst done', completed: false},
-        {item: 'I am not done either', completed: false},
-        {item: 'i am super done', completed: true},
-        {item: 'i am done too', completed: true},
-        ],
+        todos: [],
     },
     methods: {
         addNewTodo: function () {
             this.todos.push({
               item: this.newTodoText,
               completed: false,
-            })
+            },)
             this.newTodoText = ''
         },
         removeTodo(todoItem) {
             // Remove item at its index in the array
-            this.todos.splice(this.todos.indexOf(todoItem), 1);
+            let myIndex = this.todos.indexOf(todoItem)
+            this.todos.splice(myIndex, 1);
         },
         markComplete(index) {
             this.todos[index].completed = true;
@@ -37,6 +30,7 @@ new Vue({
         },
         completeTodos() {
             return this.todos.filter(todo => todo.completed);
-        }
-    }
-})
+        },
+    },
+},
+)
