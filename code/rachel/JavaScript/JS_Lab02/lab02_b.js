1db3630a151e848c61e76f1207df901eeab3c958
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2023-01-10 21:36:09 -0800

    JS Lab 2 In Work

---
+++ code/rachel/JavaScript/JS_Lab02/lab02_b.js
@@ -0,0 +1,52 @@
+//Blackjack advice//
+const cardValues = {a: 1, Ace: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, j: 10, jack: 10, q: 10, queen: 10, k: 10, king: 10}
+
+const body = document.querySelector('body')
+const firstCard = document.getElementById('firstCard').value
+// const submit = document.querySelector('input[type=button]')
+const firstSubmit = document.querySelector('#firstSubmit')
+
+function blackJack() {
+    // let secondCard = document.createElement('secondCard')
+    let secondCard = document.createElement('div')//add input
+    
+    secondCard.innerText = "What's your second card? "
+    //secondCard.value = ''
+    let secondSubmit = document.createElement('button')
+    body.appendChild(secondCard, secondSubmit)
+    secondSubmit.addEventListener('click', function() {
+        
+    })
+
+    //let thirdCard = document.createElement('thirdCard')
+    // let thirdCard = document.createElement('button')
+    // thirdCard.innerText = "What's your third card? "
+    // thirdCard.addEventListener('click', function() {
+    //     body.appendChild(thirdCard)
+    // })
+
+    // let value1 = cardValues[firstCard]
+    // let value2  = cardValues[secondCard]
+    // let value3 = cardValues[thirdCard]
+
+    // let cardsTotal = value1 + value2 + value3
+
+    // const advice = document.createElement('advice')
+
+    // if (cardsTotal <= 16) {
+    //     advice.innerText = `${cardsTotal} hit me`
+    //     body.appendChild(advice)
+    // } else if (cardsTotal <= 20) {
+    //     advice.innerText = `${cardsTotal} stay`
+    //     body.appendChild(advice)
+    // } else if (cardsTotal == 21) {
+    //     advice.innerText = `${cardsTotal} BLACKJACK!`
+    //     body.appendChild(advice)
+    // } else if (cardsTotal > 21) { 
+    //     advice.innerText = `${cardsTotal} busted`
+    //     body.appendChild(advice)
+    // }
+}
+
+firstSubmit.addEventListener('click', blackJack)
+    //add event listener for first card, then run blackjack function
