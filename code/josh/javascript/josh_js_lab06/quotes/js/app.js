13fe177a8eb155112dfa0e40f66de61f5c0e0f9a
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-12 11:28:26 -0800

    working on axios requests

--- code/josh/javascript/josh_js_lab06/quotes/js/app.js
+++ code/josh/javascript/josh_js_lab06/quotes/js/app.js
@@ -1,17 +1,56 @@
+// const axios = require('axios');
+
 new Vue ({
     el: '#app',
     data: {
-        header: {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
-        parameters: {'filter': keyword, 'page': page}, //stores user input and page number to allow search terms and next page functionality in URL
-        searchResponse: requests.get('https://favqs.com/api/quotes', params=parameters, headers=header),
-        searchResponseText: search_response.json(),
-        quotesList: searchResponseText['quotes'], //sorts returned list index 'quotes'
-        response: requests.get('https://favqs.com/api/qotd'),
-        responseText: response.json(),
-        author: response_text['quote']['author'],
-        quote: response_text['quote']['body'],
-    },
+        output: {}
+        },
     methods: {
-
+        getQuote() {
+            console.log('GET request');
+            axios.get('https://favqs.com/api/quotes/', {
+                headers: {
+                    'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
+                params: {
+                    // 'author': userInput,
+                    // 'body': userInput,
+                    // 'keyword': userInput,
+                    // 'author': data.quote.author,
+                    // 'tag': tag
+                }
+    }).then(res => this.output = res)
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
     },
-})
+}})
+
+
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+// Let the user enter a search term and select whether to search by keyword, author, or tag.
+// When the page first loads, show the user a set of completely random quotes.
+// You must have at least one Vue component in your app. Review components here
+// 
+// Read the API documentation!
+// Remember to use your Vue app data as your single source of truth.
+// You'll need to set the Authorization header for the FavQs API to work.
+
+            // header: {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'},
+            // parameters: {'filter': keyword, 'page': page}, //stores user input and page number to allow search terms and next page functionality in URL
+            // searchResponse: requests.get('https://favqs.com/api/quotes', params=parameters, headers=header),
+        //     searchResponseText: search_response.json(),
+        //     quotesList: searchResponseText['quotes'], //sorts returned list index 'quotes'
+        //     response: requests.get('https://favqs.com/api/qotd'),
+        //     responseText: response.json(),
+        //     author: response_text['quote']['author'],
+        //     quote: response_text['quote']['body'],
+        //     qotd: 'https://favqs.com/api/qotd'
+        // },
