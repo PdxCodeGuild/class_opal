9592eec2d77e8daf6389d1317b52c3e7677024ae
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-12 15:10:53 -0800

    removed some comments

--- code/josh/javascript/josh_js_lab06/quotes/js/app.js
+++ code/josh/javascript/josh_js_lab06/quotes/js/app.js
@@ -26,21 +26,5 @@ new Vue ({
 }})
 
 
-// Let the user enter a search term and select whether to search by keyword, author, or tag.
 // When the page first loads, show the user a set of completely random quotes.
 // You must have at least one Vue component in your app. Review components here
-// 
-// Read the API documentation!
-// Remember to use your Vue app data as your single source of truth.
-// You'll need to set the Authorization header for the FavQs API to work.
-
-            // parameters: {'filter': keyword, 'page': page}, //stores user input and page number to allow search terms and next page functionality in URL
-            // searchResponse: requests.get('https://favqs.com/api/quotes', params=parameters, headers=header),
-        //     searchResponseText: search_response.json(),
-        //     quotesList: searchResponseText['quotes'], //sorts returned list index 'quotes'
-        //     response: requests.get('https://favqs.com/api/qotd'),
-        //     responseText: response.json(),
-        //     author: response_text['quote']['author'],
-        //     quote: response_text['quote']['body'],
-        //     qotd: 'https://favqs.com/api/qotd'
-        // },
