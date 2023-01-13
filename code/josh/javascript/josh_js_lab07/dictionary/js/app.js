9538baea515ef00eda77569546cec290127a84ca
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-13 14:43:05 -0800

    added audio file redirect for dictionary words

--- code/josh/javascript/josh_js_lab07/dictionary/js/app.js
+++ code/josh/javascript/josh_js_lab07/dictionary/js/app.js
@@ -3,6 +3,7 @@ new Vue({
     data: {
         output: {},
         userInput: '',
+        audio: '',
     },
     mounted() {
     },
@@ -10,20 +11,21 @@ new Vue({
         getDefinition() {
             console.log('GET Request');
             axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.userInput}`)
-            .then(response => this.output = response.data[0].meanings[0])//.definitions//[0].definition)
+            .then(response => this.output = response.data[0].meanings[0].definitions[0].definition)
+            .then(data => console.log(data))
+            .then(() => this.getAudio())
+            .catch(error => console.error(error));
+        },
+        getAudio() {
+            console.log('GET Request');
+            axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.userInput}`)
+            .then(response => this.audio = response.data[0].phonetics[0].audio)
             .then(data => console.log(data))
             .catch(error => console.error(error));
         },
     }
 })
 
-
-
-
-
-
-// Choose any API and write a page to interact with it.
-// Your page should take in some sort of user input and return different results based on that input.API
-// Your page should offer some sort of interactivity in the results. You should also apply some sort of basic professional styling.
+// Your page should offer some sort of interactivity with the results. You should also apply some sort of basic professional styling.
 // Using a CSS framework is totally fine. Your webpage should utilize Vue to build the interface and Axios for making ajax requests.
 // https://vuejs.org/v2/cookbook/using-axios-to-consume-apis.html
