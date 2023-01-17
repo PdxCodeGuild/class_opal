27c2055f62ed3ccb01585a19c3f4b9cd5fd98ff6
NMurdaugh <nmurdaugh42@gmail.com>
2023-01-16 17:36:56 -0700

    built vue with data and computed data; started on method for retrieving kanye quote

--- code/nick/javascript/mini-capstone/js/app.js
+++ code/nick/javascript/mini-capstone/js/app.js
@@ -1,1 +1,34 @@
+new Vue({
+    el: '#app',
+    data: () => {
+        return {
+            voiceTokens: {
+                'Obama': 'TM:58vtv7x9f32c',
+                'Gandhi': 'TM:cvw5qkye9y22',
+                'Churchill': 'TM:3na2hzvbfqn7',
+                'Kennedy': 'TM:a9pmkvtg2p6b',
+                'FDR': 'TM:jh0bts33pn7x',
+                'Teddy': 'TM:pn9edma33t2j',
+            },
+            kanyeQuote: null,
+            audioPath: null,
 
+        }
+    },
+    methods: {
+        getKanyeQuote() {
+            axios.get('https://api.kanye.rest')
+            .then(res => this.kanyeQuote = res)
+        }
+    },
+    computed: {
+        uuidToken() {
+            let tokenArray = []
+            const range = 20
+            for (let i = 0; i < range; i++) {
+                tokenArray.push(Math.floor(Math.random() * 10))
+            }
+            return tokenArray.join()
+        }
+    },
+})
