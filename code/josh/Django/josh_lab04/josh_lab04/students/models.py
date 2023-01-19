722b58a5e69fe0c21f926563af3c19522d207eb5
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-19 15:43:37 -0800

    created new project and new app 'students'

---
+++ code/josh/Django/josh_lab04/josh_lab04/students/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    cohort = models.CharField(max_length=50)
+    favorite_topic = models.CharField(max_length=50)
+    favorite_teacher = models.CharField(max_length=50)
+    capstone = models.CharField(max_length=50)
+
+
+    def __str__ (self):
+        return f'''
+        Student: {self.first_name} {self.last_name}
+        Cohort: {self.cohort}
+        Favorites: Topic - {self.favorite_topic} Teacher - {self.favorite_teacher}
+        Capstone: {self.capstone}
+        '''
