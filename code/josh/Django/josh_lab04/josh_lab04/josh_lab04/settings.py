5f481f622d067938b5b91aa1db2e8115c19d3161
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-19 15:54:27 -0800

    added cors headers

--- code/josh/Django/josh_lab04/josh_lab04/josh_lab04/settings.py
+++ code/josh/Django/josh_lab04/josh_lab04/josh_lab04/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'http://localhost:3000'
+)
 
 ROOT_URLCONF = 'josh_lab04.urls'
 
