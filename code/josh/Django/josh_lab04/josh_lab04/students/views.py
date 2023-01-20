5b6c34ce7d9a2f08be79ac8b7bf9f5ab7819c301
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-19 16:27:48 -0800

    added serializers

--- code/josh/Django/josh_lab04/josh_lab04/students/views.py
+++ code/josh/Django/josh_lab04/josh_lab04/students/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from students.models import Student
+from .serializers import StudentSerializer
 
-def index(request):  
-    return HttpResponse("Hello, world")  
+
+class StudentAPIView(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CreateStudent(generics.CreateAPIView):
+    serializer_class = StudentSerializer
+
+
+class StudentView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
